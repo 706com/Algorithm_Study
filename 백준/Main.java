beb7f737c029892b751a67caff0fc9120db3db15
706com <ehdgusjdbswl@gmail.com>
2022-11-10 19:53:21 +0900

    0

--- 백준/Main.java
+++ 백준/Main.java
@@ -1,48 +1,78 @@
 package 백준;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
-        int max = 0;
-
+        long[] A = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
         for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        long[] arr = new long[M];
+
+        st = new StringTokenizer(br.readLine()," ");
+
+        for(int i=0; i<M; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            if(max<arr[i]){
-                max = arr[i];
+        }
+        //이분탐색
+        Arrays.sort(A);
+
+//        for(int i=0; i<N; i++){
+//            System.out.print(A[i] + " ");
+//        }
+//        System.out.println();
+//
+//        for(int i=0; i<N; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+//        System.out.println();
+
+        int low,mid,high;
+
+        boolean check = false;
+
+        for(int i=0; i<arr.length; i++) {
+            long key = arr[i];
+
+            low = 0;
+            mid = 0;
+            high = A.length -1;
+            check = false;
+
+            while(low <= high) {
+                mid = (low + high) / 2;
+
+                if(key == A[mid]){
+                    check = true;
+                    break;
+                }
+                else if (key < A[mid]){
+                    high = mid-1;
+                }
+                else if(key > A[mid]){
+                    low = mid+1;
+                }
+            }
+
+            if(check){
+                System.out.println("1");
+            }
+            else{
+                System.out.println("0");
             }
         }
-
-        boolean[] check = new boolean[max+1];
-        check[0] = true;
-        check[1] = true;
-
-        for(int i=2; i<Math.sqrt(check.length); i++){
-            if(check[i]){
-                continue;
-            }
-            for(int j=i*i; j< check.length; j+=i){
-                check[j] = true;
-            }
-        }
-
-        int cnt = 0;
-        for(int i=0; i<arr.length; i++){
-            if(!check[arr[i]]){
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
     }
 }
