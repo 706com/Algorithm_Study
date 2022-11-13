399270a6fbd2ca2321101a81bdb6cd5863484aea
706com <ehdgusjdbswl@gmail.com>
2022-11-13 22:40:05 +0900

    -50

---
+++ 백준/_2775_부녀회장이_될테야.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2775_부녀회장이_될테야 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while(T != 0) {
+            T--;
+            int[] people = new int[14];
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            for(int i=0; i<k; i++){
+
+            }
+
+        }
+    }
+}
