a2773f2d762fccea84a0405664c7e942078a9aa5
706com <ehdgusjdbswl@gmail.com>
2022-11-15 15:49:30 +0900

    50

--- 백준/_2775_부녀회장이_될테야.java
+++ 백준/_2775_부녀회장이_될테야.java
@@ -1,5 +1,9 @@
 package 백준;
 
+//[백준]2775 : 부녀회장이 될테야 - JAVA(자바)
+
+//<새로 알게된 것>
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,14 +15,21 @@ public class _2775_부녀회장이_될테야 {
 
         while(T != 0) {
             T--;
-            int[] people = new int[14];
+            int[] people = new int[15];
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
 
-            for(int i=0; i<k; i++){
-                if()
-
+            for(int i=0; i<=14; i++){
+                people[i] = i;
             }
+
+            for(int i=0; i<k; i++) {
+                for (int j = 2; j <= 14; j++) {
+                    people[j] = people[j-1] + people[j];
+                }
+            }
+
+            System.out.println(people[n]);
 
         }
     }
