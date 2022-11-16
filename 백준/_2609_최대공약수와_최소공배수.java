15a1b4b16b9d35ff5abf1a5f498c5b8b62ecfd68
706com <ehdgusjdbswl@gmail.com>
2022-11-17 04:22:46 +0900

    50

--- 백준/_2609_최대공약수와_최소공배수.java
+++ 백준/_2609_최대공약수와_최소공배수.java
@@ -1,9 +1,13 @@
 package 백준;
 
+//[백준]2609 : 최대공약수와 최대공배수 - JAVA(자바)
+//새로 알게된 것
+//GCD (Greatest Common Divisor) algorithm : 유클리드 호제법
+//LCM (Least Common Multiple) : a * b / gcd
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class _2609_최대공약수와_최소공배수 {
@@ -14,28 +18,24 @@ public class _2609_최대공약수와_최소공배수 {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        ArrayList Aarr = new ArrayList();
-        for(int i=1; i<=A; i++){
-            if(A % i == 0){
-                Aarr.add(i);
-            }
-        }
-        ArrayList Barr = new ArrayList();
-        for(int i=1; i<=B; i++){
-            if(B % i == 0){
-                Barr.add(i);
-            }
-        }
+        int gcd = GCD(A,B);
+        int lcm = LCM(A,B);
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
 
-        int low,mid,high;
-        int cnt = 0;
-        low = 0;
-        mid = 0;
-        high = Aarr.size()-1;
-
-        while(low<=high){
-            mid = (low+high) / 2;
-            // key = Barr.get(cnt);
+    //GCD (Greatest Common Divisor) : 유클리드 호제법 알고리즘
+    public static int GCD(int a, int b){
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
         }
+        return a;
+    }
+
+    //LCM(Least Common Multiple) : A * B / 최대공약수
+    public static int LCM(int a, int b){
+        return a * b / GCD(a,b);
     }
 }
