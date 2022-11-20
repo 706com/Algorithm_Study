16eec854d96afbc4c4c89d2a5777a3ab76b1d359
706com <ehdgusjdbswl@gmail.com>
2022-11-20 22:52:53 +0900

    0

--- 백준/Main.java
+++ 백준/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 //
-//
+
 
 class Main{
     public static void main(String[] args) throws IOException {
