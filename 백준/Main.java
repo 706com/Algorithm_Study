312f0e7b770cffadbbc4ca56b5e15a52e28c0a2a
706com <ehdgusjdbswl@gmail.com>
2022-11-23 20:01:16 +0900

    0

--- 백준/Main.java
+++ 백준/Main.java
@@ -7,26 +7,38 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 
-class Main{
+class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine()," ");
-        for(int i=0; i<N; i++){
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int result = black(arr,N,M);
 
-        for(int i=0; i<N; i++){
-            for (int j=i+1; j<N; j++){
-                for(int k=j+1; k<N; k++){
+        System.out.println(result);
+    }
 
+    public static int black(int[] arr,int N , int M) {
+        int max = 0;
+        for (int i = 0; i < N-2; i++) {
+            for (int j = i + 1; j < N-1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if(arr[i]+arr[j]+arr[k] == M){
+                        return arr[i] + arr[j] + arr[k];
+                    }
+                    else if(arr[i]+arr[j]+arr[k] < M && max < arr[i]+arr[j]+arr[k] ){
+                        max = arr[i]+arr[j]+arr[k];
+                    }
                 }
             }
         }
+        return max;
 
 
     }
