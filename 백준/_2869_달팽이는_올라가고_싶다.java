96470669f19977b24df390a302c9adde9c33dd15
706com <ehdgusjdbswl@gmail.com>
2022-11-25 04:19:41 +0900

    50

--- 백준/_2869_달팽이는_올라가고_싶다.java
+++ 백준/_2869_달팽이는_올라가고_싶다.java
@@ -1,6 +1,12 @@
 package 백준;
 //[백준]2839 : 달팽이는 올라가고 싶다 - JAVA(자바)
 
+//<새로 알게된 것>
+//시간제한 0.15초
+//시간이 짧을때는 반복문은 절대 들어가면 안되고, 수식으로 풀어야한다.
+//수식 고민하기
+//point : 정상에 올라간 후에는 미끄러지지 않는다.
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,9 +20,22 @@ public class _2869_달팽이는_올라가고_싶다 {
         int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        int snail = 0;
         int day = 0;
 
+        day = (V-B) / (A-B);    //몫 = day
+
+        //밤이 되기 전(V-B) ,
+        //(V-B) % (V-A)
+        // 나머지가 없으면(0이면) 등반 성공
+        //나머지가 있으면 한번 더 올라야 함
+        if((V-B) % (A-B) != 0){
+            day++;
+        }
+
+        System.out.println(day);
+
+    }
+}
 
 //        while(snail < V){
 //            cnt++;
@@ -26,13 +45,3 @@ public class _2869_달팽이는_올라가고_싶다 {
 //            }
 //            snail -= B;
 //        }
-        day = (V-B) / (A-B);
-        if((V-B) % (A-B) == 0){
-            System.out.println(day);
-        }
-        else{
-            System.out.println(day++);
-        }
-
-    }
-}
