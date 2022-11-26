c3c5e657171643b77fcede9f6e82721c0c14e5ee
706com <ehdgusjdbswl@gmail.com>
2022-11-26 16:33:15 +0900

    -

---
+++ 백준/_4949_균형잡힌_세상.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _4949_균형잡힌_세상 {
+     public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         while(true){
+             String str = br.readLine();
+             if(str.equals(".")){
+                 break;
+             }
+
+         }
+     }
+}
