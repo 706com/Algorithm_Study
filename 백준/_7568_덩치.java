fec1abbeb4f5101d784d86d9b53c4d211af834da
706com <ehdgusjdbswl@gmail.com>
2022-11-30 15:06:18 +0900

    0

--- 백준/_7568_덩치.java
+++ 백준/_7568_덩치.java
@@ -1,5 +1,11 @@
 package 백준;
+//[백준]4153 : 직각삼각형 - JAVA(자바)
 
+//<새로 알게된 것>
+//
+
+//<궁금한 것>
+//
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,17 +27,26 @@ public class _7568_덩치 {
             }
         }
 
+        //rank값 저장 시킬 배열 생성 (사람수만큼)
         int[] result = new int[N];
-        int rank = 1;
+
 
         for(int i=0; i<N; i++){
+            int rank = 1;
+            for(int j=0; j<N; j++) {
 
-            for(int j=i+1; j<N; j++)
-                if(arr[i][0] < arr[j][0]){
-                    if(arr[i][1] < arr[j+1][1]){
-                        rank++;
-                    }
+                //본인을 비교 할 필요는 없으므로 생략
+                if(j==i){
+                    continue;
                 }
+
+                //1. 몸무게를 비교한다 (0번지)
+                //2. 키를 비교한다 (1번지)
+                //3. 덩치등수 매기기 (rank) : 본인보다 덩치가 큰 사람이 있으면 rank가 +1 씩 늘어난다
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
             result[i] = rank;
         }
 
