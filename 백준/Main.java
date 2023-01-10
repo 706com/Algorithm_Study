de7c31c3e5c35d62dcfc83ff63b0dd6b9f762003
706com <ehdgusjdbswl@gmail.com>
2023-01-10 20:16:21 +0900

    0

--- 백준/Main.java
+++ 백준/Main.java
@@ -1,81 +1,41 @@
-/*
 package 백준;
 
+//[백준]10814 : 나이순정렬 - JAVA(자바)
 
+//<새로 알게된 것>
+// Arrays.sort 사용법 및 Comparator<T>(), compare 함수 재정의 복기
+// compare 함수 ->  양수:뒤집기 / 음수,0 : 그대로
+// 다른 조건으로 정렬이 되더라도, 입력한 순서대로 냅두어진다.
+
+//<궁금한 것>
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine()," ");
-
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int temp = 0;
-        boolean check = false;
-
-        Loop :
-        for(int i=0; i<N-2; i++){
-            for(int j=i+1; j<N-1; j++){
-                for(int k=j+1; k<N; k++) {
-                    if (arr[i] + arr[j] + arr[k] == M) {
-                        System.out.println(M);
-                        check = true;
-                        break Loop;
-                    }
-                    else if (temp < arr[i] + arr[j] + arr[k] && arr[i] + arr[j] + arr[k] < M) {
-                        temp = arr[i] + arr[j] + arr[k];
-                    }
-                }
+        //배열에 나이와 이름 입력
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = st.nextToken();
             }
         }
-        if(!check) {
-            System.out.println(temp);
-        }
+
+        Arrays.sort(arr,new Comparator<String[]>(){
+            @Override
+            public int compare(String[] s1,String[] s2){
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
     }
 }
-*/
-
-package 백준;
-//
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-
-        int day = (V-B) / (A-B);
-        if((V-B) % (A-B) == 0){
-            System.out.println(day);
-        }
-        else{
-            System.out.println(day+1);
-        }
-    }
-}
-
-
-
-
