9940740fac246984145eea2bd2fa0bc6bf07a9de
706com <ehdgusjdbswl@gmail.com>
2023-01-12 15:56:10 +0900

    0

--- 백준/_10799_쇠막대기.java
+++ 백준/_10799_쇠막대기.java
@@ -2,7 +2,7 @@ package 백준;
 //[백준]10799 : 쇠막대기 - JAVA(자바)
 
 //<새로 알게된 것>
-//
+// 스택문제의 문제해결력..!
 
 //<궁금한 것>
 //
@@ -15,11 +15,37 @@ import java.util.Stack;
 
 public class _10799_쇠막대기 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
 
-    }
+        int sum = 0;
 
-    public static int stackStick(String str){
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
 
+            if(c == '('){
+                stack.push(c);
+            }
+
+            // ')' 가 들어왔을 경우 판단할 2가지
+            // => 레이저의 닫기의 ')' 인가? OR 쇠막대기 닫기의 ')' 인가?
+            // 1) 레이저 닫기의 ')' 일 경우 직전 괄호가 '('
+            //  -> 스택에 남아있는 ( 의 갯수 더해주기
+            // 2) 쇠막대기 닫기의 ')' 일 경우 직전 괄호가 ')'
+            //  -> 닫히는 괄호이기 때문애 끝난지점을 알리는 +1 (그림그려서 풀면 이해 쉬움)
+
+            else if (c == ')'){
+                stack.pop();
+                if(str.charAt(i-1) == '('){
+                    sum += stack.size();
+                }
+                else if(str.charAt(i-1) == ')'){
+                    sum += 1;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
 
