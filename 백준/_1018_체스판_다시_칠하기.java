21cf503326dc662a383fce5171397a9b9881198b
706com <ehdgusjdbswl@gmail.com>
2023-01-17 21:14:23 +0900

    -

--- 백준/_1018_체스판_다시_칠하기.java
+++ 백준/_1018_체스판_다시_칠하기.java
@@ -10,7 +10,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class _1018_체스판_다시_칠하기 {
+    static char[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
@@ -18,8 +21,9 @@ public class _1018_체스판_다시_칠하기 {
         int N = Integer.parseInt(st.nextToken()); //세로
         int M = Integer.parseInt(st.nextToken()); //가로
 
-        char[][] arr = new char[N][M];
-        int count = 0;
+        arr = new char[N][M];
+
+        int result = 64;
 
         //배열에 입력받기
         for(int i=0; i<N; i++){
@@ -29,22 +33,81 @@ public class _1018_체스판_다시_칠하기 {
             }
         }
 
-        for(int i=0; i+8<N; i+=2){
-            String str = br.readLine();
-            for(int j=0; j+8<M; j+=2){
-                if(arr[i][j] != 'B'){
-                    count +=1;
+        //8x8로 잘라서 보내기
+        for(int i=0; i<=N-8; i++){
+            for(int j=0; j<=M-8; j++){
+                result = Math.min(result ,paintChess(i,j));
+                System.out.println();
+            }
+        }
+        System.out.println(result);
 
+    }
+    public static int paintChess(int startN, int startM){
+        char WB = ' ';
+        int count = 0;
+
+        int endN = startN + 8;
+        int endM = startM + 8;
+
+        System.out.println(arr[startN][startM]);
+        //잘린 체스판의 처음이 'B' 일 때.
+        if(arr[startN][startM] == 'B') {
+            for (int i = startN; i < endN; i++) {
+                //짝수번째(0,2,4,6)의 처음은 = B 이어야함
+                if ((i-startN)%2 == 0) {
+                    WB = 'B';
                 }
+                //홀수번째(1,3,5,7)의 처음은 = W 이어야함
+                else if ((i-startN) % 2 == 1) {
+                    WB = 'W';
+                }
+                for (int j = startM; j < endM; j++) {
+                    if (arr[i][j] != WB) {
+                        count++;
+                    }
 
+                    if (WB == 'B') {
+                        WB = 'W';
+                    } else if (WB == 'W') {
+                        WB = 'B';
+                    }
+                }
             }
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
+        //잘린 체스판의 처음이 'W' 일 때.
+        else if(arr[startN][startM] == 'W') {
+            for (int i = startN; i < endN; i++) {
+                //짝수번째(0,2,4,6)의 처음은 = W 이어야함
+                if ((i-startN) % 2 == 0) {
+                    WB = 'W';
+                }
+                //홀수번째(1,3,5,7)의 처음은 모두 B 이어야함
+                else if ((i-startN) % 2 == 1) {
+                    WB = 'B';
+                }
+                for (int j = startM; j < endM; j++) {
+                    if (arr[i][j] != WB) {
+                        count++;
+                    }
+                    if (WB == 'B') {
+                        WB = 'W';
+                    }
+                    else if (WB == 'W') {
+                        WB = 'B';
+                    }
+                }
+            }
+        }
+
+        for(int i=startN; i<endN; i++){
+            for(int j=startM; j<endM; j++){
                 System.out.print(arr[i][j]);
             }
             System.out.println();
         }
+        System.out.println(count);
+        return count;
     }
 }
