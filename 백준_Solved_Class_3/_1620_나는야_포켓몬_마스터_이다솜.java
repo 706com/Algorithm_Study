b644e24af5a3882f07aa62842ab8a9858a82bef3
706com <ehdgusjdbswl@gmail.com>
2023-02-18 18:39:22 +0900

    0

--- 백준_Solved_Class_3/_1620_나는야_포켓몬_마스터_이다솜.java
+++ 백준_Solved_Class_3/_1620_나는야_포켓몬_마스터_이다솜.java
@@ -2,10 +2,11 @@ package 백준_Solved_Class_3;
 //[백준]1620: 나는야 포켓몬 마스터 이다솜 - JAVA(자바)
 
 //<새로 알게된 것>
-//try-catch 구문
+//try-catch 구문 (예외처리)
+//value를 통한 key찾는 방법은 힘듬. (차라리 2개 만들기)
 
 //<궁금한 것>
-//예외처리 공부해보기
+//
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,35 +18,33 @@ public class _1620_나는야_포켓몬_마스터_이다솜 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        HashMap<Integer,String> hash = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer,String> integerKeyHash = new HashMap<>();
+        HashMap<String,Integer> stringKeyHash = new HashMap<>();
 
         int N = Integer.parseInt(st.nextToken());   //포켓몬의 개수 1<=N<=100,000
         int M = Integer.parseInt(st.nextToken());   //맞춰야 하는 문제의 개수 1<=M<=100,000
 
         //입력
         for(int i=1; i<=N; i++){
-            hash.put(i, br.readLine());
+            String name = br.readLine();
+            integerKeyHash.put(i, name);    //번호, 이름
+            stringKeyHash.put(name,i);      //이름 , 번호
         }
-
-//        for(int i=0; i<N; i++){
-//            System.out.println(hash.get(i));
-//        }
 
         //문제 맞추기
         for(int i=0; i<M; i++){
             String question = br.readLine();
+            //입력받은 값이 숫자면
             try {
                 Integer.parseInt(question);
-                System.out.println(hash.get(Integer.parseInt(question)));
+                sb.append(integerKeyHash.get(Integer.parseInt(question))).append("\n");
+
+            //입력받은 값이 문자면
             }catch(NumberFormatException e){
-                //System.out.println("들어왔숑 "+question);
-                for(int j=1; j<=N; j++){
-                    if(hash.get(j).equals(question)){
-                        System.out.println(j);
-                        break;
-                    }
-                }
+                sb.append(stringKeyHash.get(question)).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
