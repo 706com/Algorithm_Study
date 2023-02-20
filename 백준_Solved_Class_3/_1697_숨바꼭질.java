6817ef7f14dfaa96116e5e621ae799cb2b1f2d44
706com <ehdgusjdbswl@gmail.com>
2023-02-20 17:47:59 +0900

    0

--- 백준_Solved_Class_3/_1697_숨바꼭질.java
+++ 백준_Solved_Class_3/_1697_숨바꼭질.java
@@ -2,6 +2,8 @@ package 백준_Solved_Class_3;
 //[백준]1697 : 숨바꼭질 - JAVA(자바)
 
 //<새로 알게된 것>
+// dist[nx] = dist[x]++ 하면 증가되는 값이 dist[x] 이다. (dist[nx] 가 아님)
+// dist[nx] = dist[x]+1 라고 해야함
 
 //<궁금한 것>
 
@@ -28,22 +30,23 @@ public class _1697_숨바꼭질 {
 
         Arrays.fill(dist,-1);
 
-        int result = bfs(N);
+        if(N==K){
+            System.out.println("0");
+        }
+        else {
+            int result = bfs(N);
+            System.out.println(result);
+        }
 
-        System.out.println(result);
     }
     public static int bfs(int n){
         Queue<Integer> q = new LinkedList<>();
-
-        int count = 0;
         q.offer(n);
-        dist[n] = count;
-
+        dist[n] = 0;
 
         while(!q.isEmpty()) {
             int x = q.poll();
-            System.out.println("n = "+x + " ,count "+ count);
-            count++;
+//            System.out.println("n = "+x + " ,dist "+ dist[x]);
             for (int i = 0; i < 3; i++) {
                 int nx;
                 //좌 or 우 1칸 이동 or *2 이동
@@ -58,25 +61,19 @@ public class _1697_숨바꼭질 {
                 }
 
                 if(nx == K){
-                    dist[nx] = count;
+                    dist[nx] = dist[x]+1;
+//                    System.out.println("last : n = "+ nx + " ,dist "+ dist[nx]);
                     return dist[nx];
                 }
-                if(n<0 || n>dist.length || dist[nx] != -1){
+                if(nx < 0 || nx >= dist.length || dist[nx] != -1){
                     continue;
                 }
+
                 q.offer(nx);
-                dist[nx] = count;
+                dist[nx] = dist[x]+1;
             }
         }
 
         return -1;
-    }
-    static class Node{
-        int where;
-        int dist;
-        Node(int where, int dist){
-            this.where = where;
-            this.dist = dist;
-        }
     }
 }
