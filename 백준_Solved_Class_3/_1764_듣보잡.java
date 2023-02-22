7a3be47d3a26ddb0059887dad54a517dda0b3179
706com <ehdgusjdbswl@gmail.com>
2023-02-22 19:59:09 +0900

    -

---
+++ 백준_Solved_Class_3/_1764_듣보잡.java
@@ -0,0 +1,55 @@
+package 백준_Solved_Class_3;
+//[백준]1764 : 듣보잡 - JAVA(자바)
+
+//<새로 알게된 것>
+//박성배 재수업슴
+
+//<궁금한 것>
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class _1764_듣보잡 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());   //듣도 못한 사람의 수 1<=N<=500,000
+        int M = Integer.parseInt(st.nextToken());   //보도 못한 사람의 수 1<=M<=500,000
+
+        List<String> arr = new ArrayList<>();
+        List<String> result =new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            arr.add(br.readLine());
+        }
+
+
+        int count = 0;
+        for(int i=0; i<M; i++){
+            String check = br.readLine();
+            if(arr.contains(check)){
+                count++;
+                result.add(check);
+            }
+        }
+
+        Collections.sort(result);
+
+        sb.append(count).append("\n");
+
+        for(int i=0; i<result.size(); i++){
+            sb.append(result.get(i)).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
