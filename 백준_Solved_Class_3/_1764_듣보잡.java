d621472a8301d377e94afe1e7a49143224933d30
706com <ehdgusjdbswl@gmail.com>
2023-02-27 20:38:19 +0900

    0

--- 백준_Solved_Class_3/_1764_듣보잡.java
+++ 백준_Solved_Class_3/_1764_듣보잡.java
@@ -2,7 +2,7 @@ package 백준_Solved_Class_3;
 //[백준]1764 : 듣보잡 - JAVA(자바)
 
 //<새로 알게된 것>
-//박성배 재수업슴
+// Hash 가 list 보다 시간이 훨씬 단축될 수 있다.
 
 //<궁금한 것>
 
@@ -15,32 +15,35 @@ import java.util.*;
 public class _1764_듣보잡 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         HashMap<String,Integer> hash = new HashMap<>();
+
+        List<String> result = new ArrayList<>();
 
         int N = Integer.parseInt(st.nextToken());   //듣도 못한 사람의 수 1<=N<=500,000
         int M = Integer.parseInt(st.nextToken());   //보도 못한 사람의 수 1<=M<=500,000
 
-
-
+        //듣도 못한사람 입력
         for(int i=0; i<N; i++){
-            hash.put(br.readLine(),1);
+            hash.put(br.readLine(),i);
         }
 
-
         int count = 0;
+
+        //보도 못한 사람의 수 검색
         for(int i=0; i<M; i++){
             String check = br.readLine();
-            if(arr.contains(check)){
-                count++;
+
+            if(hash.containsKey(check)){
                 result.add(check);
             }
         }
 
         Collections.sort(result);
 
-        sb.append(count).append("\n");
+        sb.append(result.size()).append("\n");
 
         for(int i=0; i<result.size(); i++){
             sb.append(result.get(i)).append("\n");
