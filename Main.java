fbfc91345a86176773a865f8aa2e300ef73e0409
706com <ehdgusjdbswl@gmail.com>
2023-03-28 19:35:48 +0900

    -

--- Main.java
+++ Main.java
@@ -1,23 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 class src {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        /**
-         * 0~4 :0
-         * 5~9 : 1
-         * 10~14 :2
-         */
-        int result = 0;
-        while(N>=5){
-            result += N/5;
-            N /= 5;
+
+        for(int i=0; i<N; i++){
+            Stack<Character> stack = new Stack<>();
+            String str = br.readLine();
+            boolean check = false;
+
+            for(int j=0; j<str.length(); j++) {
+                char c = str.charAt(j);
+
+                if(c=='('){
+                    stack.push(c);
+                    continue;
+                }
+
+                else if(c==')'){
+                    if(stack.isEmpty()) {
+                        System.out.println("NO");
+                        check = true;
+                        break;
+                    }
+                    stack.pop();
+                }
+
+            }
+            if(!check) {
+                if (stack.isEmpty()) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
         }
-        System.out.println(result);
     }
 }
