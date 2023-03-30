ee44a98d6496f37f97ffcd2fa9d1c996cca043a2
706com <ehdgusjdbswl@gmail.com>
2023-03-30 17:33:49 +0900

    0

--- Main.java
+++ Main.java
@@ -1,44 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 class src {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            map.put(value, map.getOrDefault(value, 0) + 1);
+        }
 
-        for(int i=0; i<N; i++){
-            Stack<Character> stack = new Stack<>();
-            String str = br.readLine();
-            boolean check = false;
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            int result = Integer.parseInt(st.nextToken());
 
-            for(int j=0; j<str.length(); j++) {
-                char c = str.charAt(j);
-
-                if(c=='('){
-                    stack.push(c);
-                    continue;
-                }
-
-                else if(c==')'){
-                    if(stack.isEmpty()) {
-                        System.out.println("NO");
-                        check = true;
-                        break;
-                    }
-                    stack.pop();
-                }
-
-            }
-            if(!check) {
-                if (stack.isEmpty()) {
-                    System.out.println("YES");
-                } else {
-                    System.out.println("NO");
-                }
+            if (map.containsKey(result)) {
+                sb.append("1").append('\n');
+            } else {
+                sb.append('0').append('\n');
             }
         }
+        System.out.println(sb);
     }
 }
--- 백준/_1920_수찾기.java
+++ 백준/_1920_수찾기.java
@@ -4,12 +4,12 @@ package 백준;//1920 : 수찾기
 //StringTokenizer , nextToken()
 //
 
+//+ hashmap 으로 풀 수 있음
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class _1920_수찾기 {
     public static void main(String[] args) throws IOException {
@@ -77,3 +77,40 @@ public class _1920_수찾기 {
         return false;
     }
 }
+
+
+//import java.io.BufferedReader;
+//        import java.io.IOException;
+//        import java.io.InputStreamReader;
+//        import java.util.HashMap;
+//        import java.util.Map;
+//        import java.util.Stack;
+//        import java.util.StringTokenizer;
+//
+//class src {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int N = Integer.parseInt(br.readLine());
+//        Map<Integer, Integer> map = new HashMap<>();
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        for (int i = 0; i < N; i++) {
+//            int value = Integer.parseInt(st.nextToken());
+//            map.put(value, map.getOrDefault(value, 0) + 1);
+//        }
+//
+//        int M = Integer.parseInt(br.readLine());
+//        st = new StringTokenizer(br.readLine(), " ");
+//        for (int i = 0; i < M; i++) {
+//            int result = Integer.parseInt(st.nextToken());
+//
+//            if (map.containsKey(result)) {
+//                sb.append("1").append('\n');
+//            } else {
+//                sb.append('0').append('\n');
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//}
