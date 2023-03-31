1d68f8b9b4eb0279f323c8765010931afdbdffdd
706com <ehdgusjdbswl@gmail.com>
2023-03-31 18:17:50 +0900

    100

--- 백준/_10773_제로.java
+++ 백준/_10773_제로.java
@@ -5,6 +5,8 @@ package 백준;
 //char 은 비교할 때 != '0' 형식 ' ' 작은 따옴표
 //String은 비교할 때 equals 형식 " " 큰 따옴표
 
+//+간단한 스택구현으로 해결.
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,43 +16,70 @@ import java.util.StringTokenizer;
 public class _10773_제로 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int K = Integer.parseInt(br.readLine());
-
-        //입력된 수 모두를 빌더에 담아서 함수에 보내기 ex) sb=4 3 0 4 , sb=1 3 5 4 0 0 ~~
-        for(int i=0; i<K; i++){
-            sb.append(br.readLine()).append(" ");
-        }
-        int result = stackSum(K, sb.toString());
-
-        System.out.println(result);
-
-    }
-    public static int stackSum(int K, String sb){
-        //띄어쓰기 단위로 토큰화
-        StringTokenizer st = new StringTokenizer(sb," ");
         Stack<Integer> stack = new Stack<>();
 
         for(int i=0; i<K; i++){
-            //
-            String str = st.nextToken();
-
-            //스택을 이용하여 0이 아니면 int 형변환 후 push , 0이면 pop
-            if(!str.equals("0")){
-                stack.push(Integer.parseInt(str));
+            int value = Integer.parseInt(br.readLine());
+            if(value != 0){
+                stack.push(value);
+                continue;
             }
-            else if(str.equals("0")) {
-                stack.pop();
-            }
+            stack.pop();
         }
 
         int sum = 0;
-        int size = stack.size();
-
-        //sum 에 담아서 리턴
-        for(int i=0; i<size; i++){
+        while(!stack.isEmpty()){
             sum += stack.pop();
         }
-        return sum;
+        System.out.println(sum);
+
     }
 }
+
+
+
+
+
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int K = Integer.parseInt(br.readLine());
+//
+//        //입력된 수 모두를 빌더에 담아서 함수에 보내기 ex) sb=4 3 0 4 , sb=1 3 5 4 0 0 ~~
+//        for(int i=0; i<K; i++){
+//            sb.append(br.readLine()).append(" ");
+//        }
+//        int result = stackSum(K, sb.toString());
+//
+//        System.out.println(result);
+//
+//    }
+//    public static int stackSum(int K, String sb){
+//        //띄어쓰기 단위로 토큰화
+//        StringTokenizer st = new StringTokenizer(sb," ");
+//        Stack<Integer> stack = new Stack<>();
+//
+//        for(int i=0; i<K; i++){
+//            //
+//            String str = st.nextToken();
+//
+//            //스택을 이용하여 0이 아니면 int 형변환 후 push , 0이면 pop
+//            if(!str.equals("0")){
+//                stack.push(Integer.parseInt(str));
+//            }
+//            else if(str.equals("0")) {
+//                stack.pop();
+//            }
+//        }
+//
+//        int sum = 0;
+//        int size = stack.size();
+//
+//        //sum 에 담아서 리턴
+//        for(int i=0; i<size; i++){
+//            sum += stack.pop();
+//        }
+//        return sum;
+//    }
