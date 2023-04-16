2e873a03ce5fccf9cf66ce691c47e2b9b72192ca
706com <ehdgusjdbswl@gmail.com>
2023-04-17 03:36:46 +0900

    100

--- 백준_Solved_Class_3/_1764_듣보잡.java
+++ 백준_Solved_Class_3/_1764_듣보잡.java
@@ -15,41 +15,74 @@ import java.util.*;
 public class _1764_듣보잡 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        HashMap<String,Integer> hash = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
+        HashMap<String,Integer> map = new HashMap<>();
         List<String> result = new ArrayList<>();
 
-        int N = Integer.parseInt(st.nextToken());   //듣도 못한 사람의 수 1<=N<=500,000
-        int M = Integer.parseInt(st.nextToken());   //보도 못한 사람의 수 1<=M<=500,000
-
-        //듣도 못한사람 입력
         for(int i=0; i<N; i++){
-            hash.put(br.readLine(),i);
+            String listener = br.readLine();
+            map.put(listener,map.getOrDefault(listener,0) +1);
         }
 
         int count = 0;
-
-        //보도 못한 사람의 수 검색
         for(int i=0; i<M; i++){
-            String check = br.readLine();
-
-            if(hash.containsKey(check)){
-                result.add(check);
+            String viewer = br.readLine();
+            if(map.containsKey(viewer)){
+                count++;
+                result.add(viewer);
             }
         }
-
         Collections.sort(result);
-
-        sb.append(result.size()).append("\n");
-
-        for(int i=0; i<result.size(); i++){
-            sb.append(result.get(i)).append("\n");
+        System.out.println(count);
+        for(int i=0; i<result.size(); i++) {
+            System.out.println(result.get(i));
         }
-
-        System.out.println(sb);
-
     }
 }
+
+
+
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//        HashMap<String,Integer> hash = new HashMap<>();
+//
+//        List<String> result = new ArrayList<>();
+//
+//        int N = Integer.parseInt(st.nextToken());   //듣도 못한 사람의 수 1<=N<=500,000
+//        int M = Integer.parseInt(st.nextToken());   //보도 못한 사람의 수 1<=M<=500,000
+//
+//        //듣도 못한사람 입력
+//        for(int i=0; i<N; i++){
+//            hash.put(br.readLine(),i);
+//        }
+//
+//        int count = 0;
+//
+//        //보도 못한 사람의 수 검색
+//        for(int i=0; i<M; i++){
+//            String check = br.readLine();
+//
+//            if(hash.containsKey(check)){
+//                result.add(check);
+//            }
+//        }
+//
+//        Collections.sort(result);
+//
+//        sb.append(result.size()).append("\n");
+//
+//        for(int i=0; i<result.size(); i++){
+//            sb.append(result.get(i)).append("\n");
+//        }
+//
+//        System.out.println(sb);
+//
+//    }
