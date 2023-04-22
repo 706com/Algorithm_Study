9f5b458997b3c936f3618d710cee832ee8e18902
706com <ehdgusjdbswl@gmail.com>
2023-04-22 17:41:07 +0900

    50

--- 백준/Main.java
+++ 백준/Main.java
@@ -3,46 +3,31 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Stack;;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String command = br.readLine();
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        HashMap<Integer,String> keyNum = new HashMap<>();
+        HashMap<String,Integer> keyName = new HashMap<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int answer = solution(command);
-        System.out.println(answer);
-    }
-
-    static int solution(String cmd){
-        Stack<Character> stack = new Stack<>();
-        int answer = 0;
-        char before = '-';
-        //1. '(' ')' 를 구분한다.
-        for(int i=0; i<cmd.length(); i++){
-            if(i>=1){
-                before = cmd.charAt(i-1);
-            }
-            char c = cmd.charAt(i);
-            //2. '('이면 스택에 넣어주고
-            if(c == '('){
-                stack.push(c);
-            }
-            //3. ) 이면 앞이 '(' 인지 ')' 인지 확인 하고 pop과 출력을 담아준다.
-            else if( i>0 && c==')' && !stack.isEmpty()){
-                if(before == '('){
-                    stack.pop();
-                    answer += stack.size();
-                }
-                else if(before == ')'){
-                    answer +=1;
-                    stack.pop();
-                }
+        for(int i=1; i<=N; i++){
+            String name = br.readLine();
+            keyNum.put(i,name);
+            keyName.put(name,i);
+        }
+        for(int i=0; i<M; i++){
+            String answer = br.readLine();
+            try {
+                int num = Integer.parseInt(answer);
+                System.out.println(keyNum.get(num));
+            } catch(NumberFormatException num) {
+                System.out.println(keyName.get(answer));
             }
         }
-        return answer;
     }
-
-
 }
--- 백준_Solved_Class_3/_1620_나는야_포켓몬_마스터_이다솜.java
+++ 백준_Solved_Class_3/_1620_나는야_포켓몬_마스터_이다솜.java
@@ -16,35 +16,60 @@ import java.util.StringTokenizer;
 
 public class _1620_나는야_포켓몬_마스터_이다솜 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        StringBuilder sb = new StringBuilder();
-        HashMap<Integer,String> integerKeyHash = new HashMap<>();
-        HashMap<String,Integer> stringKeyHash = new HashMap<>();
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        HashMap<Integer,String> keyNum = new HashMap<>();
+        HashMap<String,Integer> keyName = new HashMap<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());   //포켓몬의 개수 1<=N<=100,000
-        int M = Integer.parseInt(st.nextToken());   //맞춰야 하는 문제의 개수 1<=M<=100,000
-
-        //입력
         for(int i=1; i<=N; i++){
             String name = br.readLine();
-            integerKeyHash.put(i, name);    //번호, 이름
-            stringKeyHash.put(name,i);      //이름 , 번호
+            keyNum.put(i,name);
+            keyName.put(name,i);
         }
-
-        //문제 맞추기
         for(int i=0; i<M; i++){
-            String question = br.readLine();
-            //입력받은 값이 숫자면
+            String answer = br.readLine();
             try {
-                Integer.parseInt(question);
-                sb.append(integerKeyHash.get(Integer.parseInt(question))).append("\n");
-
-            //입력받은 값이 문자면
-            }catch(NumberFormatException e){
-                sb.append(stringKeyHash.get(question)).append("\n");
+                int num = Integer.parseInt(answer);
+                System.out.println(keyNum.get(num));
+            } catch(NumberFormatException num) {
+                System.out.println(keyName.get(answer));
             }
         }
-        System.out.println(sb);
     }
 }
+
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//        StringBuilder sb = new StringBuilder();
+//        HashMap<Integer,String> integerKeyHash = new HashMap<>();
+//        HashMap<String,Integer> stringKeyHash = new HashMap<>();
+//
+//        int N = Integer.parseInt(st.nextToken());   //포켓몬의 개수 1<=N<=100,000
+//        int M = Integer.parseInt(st.nextToken());   //맞춰야 하는 문제의 개수 1<=M<=100,000
+//
+//        //입력
+//        for(int i=1; i<=N; i++){
+//            String name = br.readLine();
+//            integerKeyHash.put(i, name);    //번호, 이름
+//            stringKeyHash.put(name,i);      //이름 , 번호
+//        }
+//
+//        //문제 맞추기
+//        for(int i=0; i<M; i++){
+//            String question = br.readLine();
+//            //입력받은 값이 숫자면
+//            try {
+//                Integer.parseInt(question);
+//                sb.append(integerKeyHash.get(Integer.parseInt(question))).append("\n");
+//
+//                //입력받은 값이 문자면
+//            }catch(NumberFormatException e){
+//                sb.append(stringKeyHash.get(question)).append("\n");
+//            }
+//        }
+//        System.out.println(sb);
+//    }
