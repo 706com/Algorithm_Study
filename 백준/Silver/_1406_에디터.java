590fb69f006cb4e68026a54a04c5c7830c416450
706com <ehdgusjdbswl@gmail.com>
2023-04-24 00:00:58 +0900

    0

---
+++ 백준/Silver/_1406_에디터.java
@@ -0,0 +1,16 @@
+package 백준.Silver;
+//[백준]1406 : 에디터 - JAVA(자바)
+
+//< 나의 알고리즘 >
+
+//< 답안 알고리즘 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+public class _1406_에디터 {
+    public static void main(String[] args) {
+
+    }
+}
