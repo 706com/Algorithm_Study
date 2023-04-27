573816465e8a5bc8aaeb46cbf835c0d435961c7c
706com <ehdgusjdbswl@gmail.com>
2023-04-27 16:43:33 +0900

    -

--- 백준/Main.java
+++ 백준/Main.java
@@ -4,30 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine()," ");
-        HashMap<Integer,String> keyNum = new HashMap<>();
-        HashMap<String,Integer> keyName = new HashMap<>();
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        for(int i=1; i<=N; i++){
-            String name = br.readLine();
-            keyNum.put(i,name);
-            keyName.put(name,i);
-        }
-        for(int i=0; i<M; i++){
-            String answer = br.readLine();
-            try {
-                int num = Integer.parseInt(answer);
-                System.out.println(keyNum.get(num));
-            } catch(NumberFormatException num) {
-                System.out.println(keyName.get(answer));
+        int N= Integer.parseInt(br.readLine());
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        while(!st.isEmpty()){
+            int num = Integer.parseInt(br.readLine());
+            if(st.peek() < num && !st.isEmpty()){
+                for(int i=st.peek()+1; i<=num; i++){
+                    st.push(i);
+                }
             }
+
+            else if(st.peek() > num){
+
+            }
+            int before = num;
         }
+
     }
 }
