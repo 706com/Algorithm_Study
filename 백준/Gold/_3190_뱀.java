69200c4d329d56df03a9d7fb193b404c764ad371
706com <ehdgusjdbswl@gmail.com>
2023-05-04 13:53:26 +0900

    -

---
+++ 백준/Gold/_3190_뱀.java
@@ -0,0 +1,22 @@
+package 백준.Gold;
+//[백준]3190 : 뱀 - JAVA(자바)
+
+//< 나의 알고리즘 >
+
+//< 답안 알고리즘 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _3190_뱀 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    //보드 크기
+        int K = Integer.parseInt(br.readLine());    //사과의 갯수
+    }
+}
