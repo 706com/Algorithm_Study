e3043ff6eb0b4b513a10b17276de798855743267
706com <ehdgusjdbswl@gmail.com>
2023-05-10 17:29:00 +0900

    -

--- 백준/Silver/_1021_회전하는큐.java
+++ 백준/Silver/_1021_회전하는큐.java
@@ -1,7 +1,10 @@
 package 백준.Silver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class _1021_회전하는큐 {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
