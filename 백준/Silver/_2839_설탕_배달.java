e266f2f1fd47ae4e9311b678fd8253349d183c2e
706com <ehdgusjdbswl@gmail.com>
2023-05-17 01:41:35 +0900

    0

---
+++ 백준/Silver/_2839_설탕_배달.java
@@ -0,0 +1,24 @@
+package 백준.Silver;
+//[백준]2839 : 설탕 배달 - JAVA(자바)
+
+//< 나의 알고리즘 >
+//
+
+//< 답안 알고리즘 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2839_설탕_배달 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+}
