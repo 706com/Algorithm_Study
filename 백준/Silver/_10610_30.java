dc294f2f2cffd5bd12b69f292762cdabf9162cfe
706com <ehdgusjdbswl@gmail.com>
2023-05-29 17:40:42 +0900

    0

--- 백준/Silver/_10610_30.java
+++ 백준/Silver/_10610_30.java
@@ -15,22 +15,45 @@ package 백준.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class _10610_30 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        boolean zero = false;
-        long sum =0;
+        int[] arr = new int[str.length()];
+        long sum = 0;
 
+        //배열에 값 넣기
         for(int i=0; i<str.length(); i++){
             char c = str.charAt(i);
-            if(c=='0'){
-                zero = true;
-            }
-
-
+            arr[i] = Character.getNumericValue(c);
         }
+
+        //오름차순 정렬
+        Arrays.sort(arr);
+
+        //모든 값 더하기
+        for(int i=0; i<str.length(); i++){
+            sum += arr[i];
+        }
+
+        //조건 1,2 중 한개라도 부합하지 않으면 -1 출력 후 리턴
+        if(!str.contains("0") || sum % 3 != 0){
+            System.out.println("-1");
+            return;
+        }
+
+
+        //반대로 돌면서 뒤부터 builder에 넣기
+        StringBuilder sb = new StringBuilder();
+        for(int i=arr.length-1; i>=0; i--){
+            sb.append(arr[i]);
+        }
+
+        System.out.println(sb);
+
+
     }
 }
