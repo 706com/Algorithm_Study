57b31a7b97a2e21b6dd4f87599b89a9c33642a2e
706com <ehdgusjdbswl@gmail.com>
2023-05-29 21:16:06 +0900

    0

--- 백준/Silver/_1912_연속합.java
+++ 백준/Silver/_1912_연속합.java
@@ -1,49 +1,46 @@
 package 백준.Silver;
-//[백준]11053 : 가장 긴 증가하는 부분 수열 - JAVA(자바)
+//[백준]1932 : 연속합 - JAVA(자바)
 
-//< 나의 알고리즘 >
+//< 알고리즘 유형 >
+// DP
 
-//< 답안 알고리즘 >
-// DP 문제.
-// == 최장 증가 부분 수열 (LIS) 라고도 한다.
-// DP 임에도 불구하고, 시간복잡도가 O(N^2) 이다.
+//< 알고리즘 풀이 >
+// LIS 와 비슷해보인다. 연속합이 음수가 되지 않는 구간으로 설정하여 나아가기 (X)
+// LIS 가 아닌,,, 그냥 메모리제이션으로 앞서 누적해나온 값과 비교하면 끝..
 
 //< 새로 알게된 것 >
 
 //< 궁금한 것 >
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class _11053_가장_긴_증가하는_부분_수열 {
+public class _1912_연속합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+
         int[] arr = new int[N];
         int[] dp = new int[N];
 
-        //배열에 값들 넣기
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //LIS
-        for(int i=0; i<N; i++){
-            dp[i] = 1;
-            for(int j=0; j<i; j++){
-                if(arr[j] < arr[i] && dp[j] >= dp[i]){
-                    dp[i] = dp[j]+1;
-                }
-            }
+        dp[0] = arr[0];
+
+        for(int i=1; i<N; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i] ,arr[i]);
         }
 
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<N;i++){
-            if(max<dp[i]){
+            if(max < dp[i]){
                 max = dp[i];
             }
         }
---
+++ 백준/Silver/_1932_정수_삼각형.java
@@ -0,0 +1,61 @@
+package 백준.Silver;
+//[백준]1932 : 정수 삼각형 - JAVA(자바)
+
+//< 알고리즘 유형 >
+// DP
+
+//< 알고리즘 풀이 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1932_정수_삼각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N+1][N+1];
+        int[][] dp = new int[N+1][N+1];
+
+        for(int i=1; i<=N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int j=1;
+            while(st.hasMoreTokens()){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                j++;
+            }
+        }
+
+//        for(int i=0; i<N; i++){
+//            for(int j=0; j<arr[i].length; j++){
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+        dp[1][1] = arr[1][1];
+
+        for(int i=2; i<=N; i++){         //depth
+            for(int j=1; j<=N; j++){     //breath
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
+            }
+        }
+
+        int max = 0;
+        for(int i=1; i<=N; i++){
+            if(max< dp[N][i]){
+                max = dp[N][i];
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
