955f423762f890073f5ab46e4feec1397673b6c5
706com <ehdgusjdbswl@gmail.com>
2023-06-04 23:41:24 +0900

    -

---
+++ 프로그래머스/Lv2/의상.java
@@ -0,0 +1,23 @@
+package 프로그래머스.Lv2;
+//< 알고리즘 유형 >
+// 해쉬
+
+//< 알고리즘 풀이 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+public class 의상 {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        의상 sol = new 의상();
+        System.out.println(sol.solution(clothes));
+    }
+}
