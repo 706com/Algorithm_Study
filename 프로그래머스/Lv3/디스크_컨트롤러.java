90bba4c56945a9435421d7ca1e6695500ff03532
706com <ehdgusjdbswl@gmail.com>
2023-06-16 20:44:13 +0900

    -

--- 프로그래머스/Lv3/디스크_컨트롤러.java
+++ 프로그래머스/Lv3/디스크_컨트롤러.java
@@ -11,34 +11,62 @@ package 프로그래머스.Lv3;
 
 //< 궁금한 것 >
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class 디스크_컨트롤러 {
     public int solution(int[][] jobs) {
         int answer = 0;
-        PriorityQueue<Job> pq = new PriorityQueue<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[1]- o2[1]);
+
+        Arrays.sort(jobs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });
 
         //pq 에 클래스단위로 넣기.
-        for(int i=0; i<jobs.length; i++){
-            Job job = new Job(jobs[i][0],jobs[i][1]);
-            pq.offer(job);
-        }
 
+        int index = 0;
+        int count = 0;
+        int total = 0;
+        int end = 0;
+
+        while(count < jobs.length){
+            while(index < jobs.length && jobs[index][0] <= end){
+                pq.add(jobs[index]);
+                index++;
+            }
+
+            if(pq.isEmpty()){
+                end = jobs[index][0];
+            }
+            else{
+                int[] cur = pq.poll();
+                total += cur[1] + end - cur[0];
+                end += cur[1];
+                count++;
+            }
+        }
+        answer = total / jobs.length;
         return answer;
     }
     public static void main(String[] args) {
-        int[][] jobs = {{0,3},{1,9},{2,6}};
+//        int[][] jobs = {{0,3},{1,9},{2,6}};
+        int[][] jobs = {{0,9},{1,6},{2,3}};
 
         디스크_컨트롤러 sol = new 디스크_컨트롤러();
         sol.solution(jobs);
 
     }
-    static class Job{
-        int start;
-        int spend;
-        Job(int start, int spend){
-            this.start=start;
-            this.spend=spend;
-        }
-    }
+//    static class Job{
+//        int start;
+//        int spend;
+//        Job(int start, int spend){
+//            this.start=start;
+//            this.spend=spend;
+//        }
+//    }
 }
