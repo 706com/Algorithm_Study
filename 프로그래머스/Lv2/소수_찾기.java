c62286020e694455d979ba6d2b9376b121279cc9
706com <ehdgusjdbswl@gmail.com>
2023-08-28 08:46:22 +0900

    0

---
+++ 프로그래머스/Lv2/소수_찾기.java
@@ -0,0 +1,23 @@
+package 프로그래머스.Lv2;
+//[프로그래머스] 소수 찾기 - JAVA(자바)
+
+//< 알고리즘 유형 >
+// 완전 탐색
+
+//< 알고리즘 풀이 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+public class 소수_찾기 {
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        String numbers = "17";
+        소수_찾기 sol = new 소수_찾기();
+        sol.solution(numbers);
+    }
+}
