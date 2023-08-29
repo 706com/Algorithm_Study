9d3fdb816d30748fdfdc4f716870baf8675b1d3e
706com <ehdgusjdbswl@gmail.com>
2023-08-29 09:15:59 +0900

    .

---
+++ 문제해결력연습/_5397_키로거.java
@@ -0,0 +1,14 @@
+package 문제해결력연습;
+
+// < 알고리즘 유형 >
+// 스택
+
+// < 풀이 접근 >
+// 1. 스택 2개를 구현한다. (leftStk , rightStk)
+// 2. 문자열이 들어오는대로 left Stack에 넣는다.
+// 3. '<' 가 들어오면, 들어온 수 만큼 leftStack 에 있는 값을 rightStack 에 옮긴다.
+// 4. '>' 가 들어오면, 들어온 수 만큼 rightStack에 있는 값을 leftStack에 옮긴다.
+// 5. 모든 값을 right Stack에 옮기고 출력한다.
+
+public class _5397_키로거 {
+}
