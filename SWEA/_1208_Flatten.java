c5579fd5eb76538ab776f4f91acbaa7894b7c485
706com <ehdgusjdbswl@gmail.com>
2023-11-05 01:57:16 +0900

    review : 23-11-04

--- SWEA/_1208_Flatten.java
+++ SWEA/_1208_Flatten.java
@@ -8,36 +8,30 @@ import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class _1208_Flatten {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = 1;
 
-        int T = 10;
-        int count = 1;
-
-        while(count<=T){
+        while(T<=10) {
             int N = Integer.parseInt(br.readLine());
-            int result;
+
+            int[] arr = new int[100];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int len = st.countTokens();
-            int[] arr = new int[len];
-
-            for(int i=0; i<len; i++){
+            for(int i=0; i<100; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(arr);
-            for(int i=0; i<N; i++){
-                arr[len-1]--;
-                arr[0]++;
+            while(N-->0) {
                 Arrays.sort(arr);
+                arr[0]++;
+                arr[99]--;
             }
+            Arrays.sort(arr);
+            int result = arr[99]-arr[0];
 
-            result = arr[len-1] - arr[0];
-
-            System.out.printf("#%d %d",count,result);
-            System.out.println();
-            count++;
+            System.out.printf("#%d %d\n",T,result);
+            T++;
         }
     }
 }
