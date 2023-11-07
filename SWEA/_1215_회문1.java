e11cd2738c1f31763e7be9674c28160b42cd1e6e
706com <ehdgusjdbswl@gmail.com>
2023-11-07 13:19:03 +0900

    review : 23-11-07

--- SWEA/_1215_회문1.java
+++ SWEA/_1215_회문1.java
@@ -5,83 +5,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _1215_회문1 {
-    static int[][] arr;
-    static int N,L;
+    static int len;
     static int result;
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = 10;
         int count = 1;
 
-        while(count<=10) {
-            L = Integer.parseInt(br.readLine());
-            N = 8;
+        while(count<=T){
+            len = Integer.parseInt(br.readLine());
             result = 0;
+            int N = 8;
+            char[][] arr = new char[N][N];
 
-            arr = new int[N][N];
-
-            for(int i=0; i<N; i++) {
+            for(int i=0; i<N; i++){
                 String str = br.readLine();
-                for(int j=0; j<N; j++) {
+                for(int j=0; j<N; j++){
                     arr[i][j] = str.charAt(j);
                 }
             }
 
-            for(int i=0; i<N; i++) {
-                searchRow(i);
-                searchCol(i);
+            //가로
+            for(int i=0; i<N; i++){
+                search(arr[i]);
             }
+            //세로
+            char[] newArr = new char[N];
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    newArr[j] = arr[j][i];
+                }
+                search(newArr);
+            }
+
             System.out.printf("#%d %d\n",count,result);
             count++;
         }
     }
-
-    static void searchRow(int l) {
-
-        int k;
-        for(int i=0; i<=N-L ; i++) {
+    public static void search(char[] arr){
+        for(int i=0; i<=8-len; i++){
             boolean check = true;
-            k = i+L-1;
+            int start = i;
+            int end = i+len-1;
 
-            for(int j=i; j<i+4; j++) {
-//				System.out.println("i "+i +" j " +j+ " k "+k);
-                if(j<k){
-                    if(arr[l][j] != arr[l][k]) {
-                        check = false;
-                    }
-                    k--;
+            for(int j=0; j<=len/2; j++) {
+                if (arr[start] != arr[end]) {
+                    check = false;
+                    break;
                 }
+                start++;
+                end--;
             }
-            if(check) {
+
+            if(check){
                 result++;
             }
         }
-//		System.out.println(result);
     }
-
-    static void searchCol(int l) {
-
-        int k;
-        for(int i=0; i<=N-L ; i++) {
-            boolean check = true;
-            k = i+L-1;
-
-            for(int j=i; j<i+4; j++) {
-//				System.out.println("i "+i +" j " +j+ " k "+k);
-                if(j<k){
-                    if(arr[j][l] != arr[k][l]) {
-                        check = false;
-                    }
-                    k--;
-                }
-            }
-            if(check) {
-                result++;
-            }
-        }
-//		System.out.println(result);
-    }
-
 }
+
