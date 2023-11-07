0f5d7e7dd16ee3a9fd1185796d7bf801b57dfe1c
706com <ehdgusjdbswl@gmail.com>
2023-11-07 12:48:04 +0900

    review : 23-11-07

--- SWEA/_1289_원재의_메모리_복구하기.java
+++ SWEA/_1289_원재의_메모리_복구하기.java
@@ -7,36 +7,21 @@ import java.io.InputStreamReader;
 public class _1289_원재의_메모리_복구하기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
         int count = 1;
-
-        while(count<=T) {
+        while(count<=T){
             String str = br.readLine();
             int N = str.length();
 
-            boolean[] arr = new boolean[N];
-
-
-            for(int i=0; i<N; i++) {
-                if(str.charAt(i) == '0') {
-                    arr[i] = false;
-                }
-                else {
-                    arr[i] = true;
-                }
-            }
-
-
+            char memory = '0';
             int result = 0;
-            boolean bit = false;
-
-            for(int i=0; i<str.length(); i++) {
-                if(arr[i] != bit) {
-                    bit = !bit;
+            for(int i=0; i<N; i++) {
+                if(str.charAt(i) != memory){
                     result++;
+                    memory = str.charAt(i);
                 }
             }
+
             System.out.printf("#%d %d\n",count,result);
             count++;
         }
