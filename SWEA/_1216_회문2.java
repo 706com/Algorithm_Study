7a5580ce81737254e94a62266b9a817166ae2d0f
706com <ehdgusjdbswl@gmail.com>
2023-11-13 19:30:50 +0900

    review : 23-11-12

--- SWEA/_1216_회문2.java
+++ SWEA/_1216_회문2.java
@@ -5,101 +5,87 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class _1216_회문2 {
-
+    static int max;
     static int N;
-    static char[][] arr;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = 10;
-        while(T-->0) {
-            int count = Integer.parseInt(br.readLine());
+        int count = 1;
+
+        while(count<=T){
+            count = Integer.parseInt(br.readLine());
+            max = 0;
             N = 100;
-            arr = new char[N][N];
+            Character[][] arr = new Character[N][N];
 
             for(int i=0; i<N; i++){
                 String str = br.readLine();
                 for(int j=0; j<N; j++){
-                    char c = str.charAt(j);
-                    arr[i][j] = c;
+                    arr[i][j] = str.charAt(j);
                 }
             }
 
-            // 가로 회문 판별
-            int max = 0;
+            //가로 찾기
             for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++) {
-                    int temp = solveRow(i,j);
-                    if(max<temp){
-                        max = temp;
-                    }
-                }
+                search(arr[i]);
             }
 
-            //세로 회문 판별
+            //세로 찾기
             for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++) {
-                    int temp = solveCol(i,j);
-                    if(max<temp){
-                        max = temp;
-                    }
+                Character[] newArr = new Character[N];
+                for(int j=0; j<N; j++){
+                    newArr[j] = arr[j][i];
                 }
+                search(newArr);
             }
-            System.out.printf("#%d %d",count,max);
-            System.out.println();
+
+            System.out.printf("#%d %d\n",count,max);
             count++;
         }
     }
 
-    //가로를 판별하는 함수
-    public static int solveRow(int l,int a){
-        int max =0;
+    static void search(Character[] arr){
 
-        for (int i = a; i < N; i++) {
-            int k = i;
-            int count = 0;
-            for (int j = a; j <= i; j++) {
-//                System.out.println(j + " " + k);
-                count++;
-                if (j <= k) {
-                    if (arr[l][j] != arr[l][k]) {
-                        count = 0;
+        //1 3 5 7 9 단위
+        for(int i=0; i<N; i++){
+            int start = i;
+            int sum = -1;
+            for(int j=0; j<N; j++) {
+                if (start-j >=0 && start+j < N) {
+                    if (arr[start - j] == arr[start + j]) {
+                        sum += 2;
+                        max = Math.max(max, sum);
+                    } else {
                         break;
                     }
                 }
-                k--;
+                else{
+                    break;
+                }
             }
-            if (max < count) {
-                max = count;
-            }
-//            System.out.println("max = " + l +"번째 "+max);
         }
-        return max;
-    }
 
-    //세로를 판별하는 함수
-    public static int solveCol(int l,int a){
-        int max =0;
-
-        for (int i = a; i < N; i++) {
-            int k = i;
-            int count = 0;
-            for (int j = a; j <= i; j++) {
-//                System.out.println(j + " " + k);
-                count++;
-                if (j <= k) {
-                    if (arr[j][l] != arr[k][l]) {
-                        count = 0;
+        //2 4 6 8 10 단위
+        for(int i=1; i<N; i++){
+            int sum = 0;
+            int start = i;
+            int end = i+1;
+            for(int j=0; j<N; j++) {
+                if (start >=0 && end < N) {
+                    if(arr[start] == arr[end]){
+                        sum+=2;
+                        max = Math.max(max,sum);
+                    }
+                    else{
                         break;
                     }
+                    start--;
+                    end++;
                 }
-                k--;
+                else{
+                    break;
+                }
             }
-            if (max < count) {
-                max = count;
-            }
-//            System.out.println("max = " + l +"번째 "+max);
         }
-        return max;
     }
 }
