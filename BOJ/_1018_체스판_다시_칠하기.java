ebea43089a5519d349c157e9d2a5ca9a89383a72
706com <ehdgusjdbswl@gmail.com>
2023-11-15 02:48:06 +0900

    -

---
+++ BOJ/_1018_체스판_다시_칠하기.java
@@ -0,0 +1,19 @@
+package BOJ;
+//[백준]1018 : 체스판 닷; 칠하기 - JAVA(자바)
+
+//<새로 알게된 것>
+
+//<궁금한 것>
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class _1018_체스판_다시_칠하기 {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
---
+++ BOJ/_6064_카잉_달력.java
@@ -0,0 +1,21 @@
+package BOJ;
+//[백준]6064 : 카잉 달력 - JAVA(자바)
+
+//< 나의 알고리즘 >
+
+//< 답안 알고리즘 >
+
+//< 새로 알게된 것 >
+
+//< 궁금한 것 >
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _6064_카잉_달력 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
