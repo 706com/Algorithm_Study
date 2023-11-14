97d45fcaee4209a49248724b753e4cd7acca20c7
706com <ehdgusjdbswl@gmail.com>
2023-11-15 03:14:24 +0900

    review : 23-11-15

--- BOJ/_1_Bronze/_2309_일곱_난쟁이.java
+++ BOJ/_1_Bronze/_2309_일곱_난쟁이.java
@@ -18,41 +18,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class _2309_일곱_난쟁이 {
-
     static int N = 9;
-    static int R = 7;   //depth
-
     static int[] arr;
     static int[] output;
     static boolean[] visited;
-    static boolean check;
-
+    static boolean check = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         arr = new int[N];
-        output = new int[R];
+        output = new int[7];
         visited = new boolean[N];
-
-        // 1. 배열에 난쟁이 키들을 입력받는다,
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        // 2. 배열을 정렬시킨다. (어차피 후에 오름차순으로 출력해야하므로)
-        Arrays.sort(arr);
 
-        // 3. combination 함수를 이용하여 sum 이 100일 경우를 찾는다.
-        combination(0,0,0);
+        dfs(0,0,0);
     }
 
-    public static void combination(int depth, int start, int sum){
-        if(depth==R){
-            if(sum==100) {
-                for (int i = 0; i < R; i++) {
+    static void dfs(int start ,int depth, int sum){
+        if(depth == 7){
+            if(sum == 100) {
+                Arrays.sort(output);
+                for(int i=0; i<7; i++){
                     System.out.println(output[i]);
                 }
+                System.out.println();
+                //여러번의 정답이 있을경우 하나만 출력
                 check = true;
-                return;
             }
             return;
         }
@@ -61,11 +54,12 @@ public class _2309_일곱_난쟁이 {
             if(!visited[i]){
                 visited[i] = true;
                 output[depth] = arr[i];
-                combination(depth+1, i+1,sum+arr[i]);
+                dfs(i,depth+1,sum+arr[i]);
                 visited[i] = false;
-                if(check){
-                    return;
-                }
+            }
+            //여러번의 정답이 있을경우 하나만 출력
+            if(check){
+                return;
             }
         }
     }
