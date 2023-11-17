3fe72cd4309ffa0d6d91ac0ef1e615678a9f96a9
706com <ehdgusjdbswl@gmail.com>
2023-11-18 01:49:31 +0900

    review : 23-11-17

--- SWEA/_1954_달팽이_숫자.java
+++ SWEA/_1954_달팽이_숫자.java
@@ -1,21 +1,8 @@
 package SWEA;
 
-// < 알고리즘 유형 >
-// 스택
-
-// < 풀이 접근 >
-// 1. 스택 2개를 구현한다. (leftStk , rightStk)
-// 2. 문자열이 들어오는대로 left Stack에 넣는다.
-// 3. '<' 가 들어오면, 들어온 수 만큼 leftStack 에 있는 값을 rightStack 에 옮긴다.
-// 4. '>' 가 들어오면, 들어온 수 만큼 rightStack에 있는 값을 leftStack에 옮긴다.
-// 5. 모든 값을 right Stack에 옮기고 출력한다.
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class _1954_달팽이_숫자 {
     static int N;
