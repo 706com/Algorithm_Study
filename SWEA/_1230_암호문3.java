c1dc263ec5729a4f18018be477b59809e54bcd0f
706com <ehdgusjdbswl@gmail.com>
2023-11-17 22:31:13 +0900

    review : 23-11-17

--- SWEA/_1230_암호문3.java
+++ SWEA/_1230_암호문3.java
@@ -1,65 +1,64 @@
 package SWEA;
 
+//16분
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class _1230_암호문3 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int count = 1;
-
-        while(count<=10) {
+        while(count<=10){
             int N = Integer.parseInt(br.readLine());
-            LinkedList<String> list = new LinkedList<>();
-
+            List<String> arr = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++) {
-                list.add(st.nextToken());
+            for(int i=0; i<N; i++){
+                arr.add(st.nextToken());
             }
 
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-
-            int index;
-            int len;
-            for(int i=0; i<M; i++) {
+            for(int i=0; i<M; i++){
                 String command = st.nextToken();
-                switch (command) {
+                int x;
+                int y;
+                String s;
+                switch (command){
                     case "I":
-                        index = Integer.parseInt(st.nextToken());
-                        len = Integer.parseInt(st.nextToken());
-                        for(int j=0; j<len; j++) {
-                            list.add(index+j, st.nextToken());
+                        x = Integer.parseInt(st.nextToken());
+                        y = Integer.parseInt(st.nextToken());
+                        for(int j=0; j<y; j++){
+                            s= st.nextToken();
+                            arr.add(x+j,s);
                         }
                         break;
                     case "D":
-                        index = Integer.parseInt(st.nextToken());
-                        len = Integer.parseInt(st.nextToken());
-                        for(int j=0; j<len; j++) {
-                            list.remove(index);
+                        x = Integer.parseInt(st.nextToken());
+                        y = Integer.parseInt(st.nextToken());
+                        for(int j=0; j<y; j++){
+                            arr.remove(x);
                         }
                         break;
                     case "A":
-                        len = Integer.parseInt(st.nextToken());
-                        for(int j=0; j<len; j++) {
-                            list.offerLast(st.nextToken());
+                        y = Integer.parseInt(st.nextToken());
+                        for(int j=0; j<y; j++) {
+                            s = st.nextToken();
+                            arr.add(arr.size()-1,s);
                         }
                         break;
                 }
             }
             System.out.printf("#%d ",count);
-            for(int i=0; i<10; i++) {
-                System.out.print(list.get(i)+" ");
+            for(int i=0; i<10; i++){
+                System.out.print(arr.get(i)+" ");
             }
             System.out.println();
             count++;
         }
-
     }
-
 }
