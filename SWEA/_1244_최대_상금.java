0206e73e51d0fa6c4dcf297dcd8881465f058eb5
706com <ehdgusjdbswl@gmail.com>
2023-11-19 01:37:32 +0900

    review : 23-11-18

--- SWEA/_1244_최대_상금.java
+++ SWEA/_1244_최대_상금.java
@@ -1,36 +1,32 @@
 package SWEA;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _1244_최대_상금 {
-
-    static char[] arr;
-
+    static int N;
     static int chance;
+    static char[] arr;
+    //static boolean[] visited;
     static int max;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
         int count = 1;
-
         while(count<=T){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String num = st.nextToken();
-
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            N = str.length();
             chance = Integer.parseInt(st.nextToken());
-            arr = new char[num.length()];
-
-            for(int i=0; i<num.length(); i++){
-                arr[i] = num.charAt(i);
-            }
-
             max = 0;
+
+            arr = new char[N];
+            //visited = new boolean[N];
+            for(int i=0; i<N; i++){
+                arr[i] = str.charAt(i);
+            }
 
             dfs(0,0);
 
@@ -38,16 +34,16 @@ public class _1244_최대_상금 {
             count++;
         }
     }
-    static void dfs(int start,int depth) {
-        if(depth == chance){
-            int num = Integer.parseInt(String.valueOf(arr));
 
+    static void dfs(int start,int depth){
+        if(depth == chance){
+            int num = Integer.valueOf(String.valueOf(arr));
             max = Math.max(max,num);
             return;
         }
 
-        for(int i=start; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++) {
+        for(int i=start; i<N; i++){
+            for(int j=i+1; j<N; j++){
                 char temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -58,7 +54,6 @@ public class _1244_최대_상금 {
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
-
         }
     }
 }
