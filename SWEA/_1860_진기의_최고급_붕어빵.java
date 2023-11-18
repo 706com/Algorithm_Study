642804ebb28d72add0402674bdefef89cc6ad0e9
706com <ehdgusjdbswl@gmail.com>
2023-11-18 21:03:06 +0900

    review : 23-11-18

--- SWEA/_1860_진기의_최고급_붕어빵.java
+++ SWEA/_1860_진기의_최고급_붕어빵.java
@@ -1,5 +1,6 @@
 package SWEA;
 
+// 12분 소요
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,7 +22,8 @@ public class _1860_진기의_최고급_붕어빵 {
             PriorityQueue<Integer> pq = new PriorityQueue<>();
 
             int max = 0;
-            //최대 시간 계산
+            // 사람 오는 순서대로 큐에 정렬
+            //+가장 늦게오는 사람이 언제인지 담아둠
             st= new StringTokenizer(br.readLine());
             for(int i=0; i<N; i++){
                 int num = Integer.parseInt(st.nextToken());
@@ -33,14 +35,15 @@ public class _1860_진기의_최고급_붕어빵 {
             int time = 0;
             int makeTime = 0;
             boolean check = true;
-            //현재 시간이 오는사람 최대까지만 반복 돌리기
+            //실제 흐른시간이 마지막 온 사람 기점까지만 알면됨
             while(time<=max){
                 // 조리시간 되면 빵 생성
                 if(makeTime==M){
                     bread+=K;
                     makeTime = 0;
                 }
-                // 손님 도착시간이 됐을 때, 빵 여부 판단
+                //예약 손님 도착시간이 됐을때, 빵이 있는지 없는지 판별
+                //->한명이라도 빵을 못먹으면 Impossible 후 종료
                 if(pq.peek() <= time){
                     if (bread <= 0) {
                         check = false;
