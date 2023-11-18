8011d5b32fb9a76bbfd69ceb80a3bf4d6e7e9ce1
706com <ehdgusjdbswl@gmail.com>
2023-11-18 11:51:20 +0900

    review : 23-11-18

--- SWEA/_2001_파리_퇴치.java
+++ SWEA/_2001_파리_퇴치.java
@@ -1,64 +1,52 @@
 package SWEA;
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+// 7분 소요
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _2001_파리_퇴치 {
-
-
     static int N,M;
     static int[][] arr;
-
-    public static void main(String[] args) throws IOException{
+    static int max;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-        int count = 1;
+        int count =1;
 
         while(count<=T){
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-
-            int sum = 0;
-            int max = 0;
+            max = 0;
 
             arr = new int[N][N];
-
             for(int i=0; i<N; i++){
-                st= new StringTokenizer(br.readLine());
+                st = new StringTokenizer(br.readLine());
                 for(int j=0; j<N; j++){
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            for(int i=0; i<=N-M; i++) {
-                for(int j=0; j<=N-M; j++) {
-                    sum = cutAndSum(i,j);
-                    if(max<sum){
-                        max = sum;
-                    }
+            for(int i=0; i<N-M+1; i++){
+                for(int j=0; j<N-M+1; j++){
+                    hit(i,j);
                 }
             }
-
-            System.out.printf("#%d %d",count, max);
+            System.out.printf("#%d %d",count,max);
             System.out.println();
             count++;
         }
     }
-
-    public static int cutAndSum(int x ,int y){
+    static void hit(int x, int y){
         int sum = 0;
-        for(int i=0; i<M; i++){
-            for(int j=0; j<M; j++){
-                sum += arr[x+i][y+j];
+        for(int i=x; i<x+M; i++){
+            for(int j=y; j<y+M; j++){
+                sum+= arr[i][j];
             }
         }
-//        System.out.println("sum " + sum);
-        return sum;
+        max = Math.max(max,sum);
     }
-
 }
