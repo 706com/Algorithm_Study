3ad80ba64d1777ec5d1b45a28b9e57956bb1c18e
706com <ehdgusjdbswl@gmail.com>
2023-12-17 20:08:20 +0900

    (review) 23-12-17 📌

--- BOJ/_2_Silver/_1406_에디터.java
+++ BOJ/_2_Silver/_1406_에디터.java
@@ -1,6 +1,9 @@
 package BOJ._2_Silver;
 //[백준]1406 : 에디터 - JAVA(자바)
 
+//소요시간 : 30분 (시간초과) -> 실패 -> 정답보기
+//소요시간 : 15분
+
 //< 나의 알고리즘 >
 // 일단은 단순 문자열+포인터로 접근하여 문제를 풀어보자.
 // 처음에 문자열로 하려 했으나, 인덱스 접근 및 넣고 빼기에 용이한 arraylist로 변경!
@@ -10,9 +13,6 @@ package BOJ._2_Silver;
 // Stack 을 이용하여 풀어야함!
 // (Right Stack , LeftStack) 을 나누어서 마치 커서가 움직이는 것 처럼 표현하기
 
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,103 +24,57 @@ public class _1406_에디터 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String str = br.readLine();
-        Stack<Character> leftSt = new Stack<>();
-        Stack<Character> rightSt = new Stack<>();
 
-        for(char c : str.toCharArray()){
-            leftSt.push(c);
+        String str = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Character> ls = new Stack<>();    // Left Stack
+        Stack<Character> rs = new Stack<>();    // Right Stack
+
+        for(int i=0; i<str.length(); i++){
+            ls.push(str.charAt(i));
         }
 
-        int M = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
-            switch(command){
-                case ("L"):
-                    if(leftSt.isEmpty()){
+            char ch;
+            switch (command){
+                case "L":
+                    if(ls.isEmpty()){
                         break;
                     }
-                    rightSt.push(leftSt.pop());
+                    ch = ls.pop();
+                    rs.push(ch);
                     break;
-                case ("D"):
-                    if(rightSt.isEmpty()){
+                case "D":
+                    if(rs.isEmpty()){
                         break;
                     }
-                    leftSt.push(rightSt.pop());
+                    ch = rs.pop();
+                    ls.push(ch);
                     break;
-                case ("B"):
-                    if(leftSt.isEmpty()){
+                case "B":
+                    if(ls.isEmpty()){
                         break;
                     }
-                    leftSt.pop();
+                    ls.pop();
                     break;
-                case ("P"):
-                    char c = st.nextToken().charAt(0);
-                    leftSt.push(c);
+                case "P":
+                    ch = st.nextToken().charAt(0);
+                    ls.push(ch);
                     break;
             }
         }
-
-        while(!leftSt.isEmpty()){
-            rightSt.push(leftSt.pop());
+        int size = ls.size();
+        for(int i=0; i<size; i++){
+            rs.push(ls.pop());
         }
 
-        while(!rightSt.isEmpty()){
-            sb.append(rightSt.pop());
+        size = rs.size();
+        for(int i=0; i<size; i++){
+            sb.append(rs.pop());
         }
         System.out.println(sb);
     }
 }
-
-
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        String str = br.readLine();
-//        ArrayList<Character> answer = new ArrayList<>();
-//
-//        for(char c : str.toCharArray()){
-//            answer.add(c);
-//        }
-//
-//        int cursor = answer.size(); //N
-//        int M = Integer.parseInt(br.readLine());
-//
-//        for(int i=0; i<M; i++){
-//            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-//            String command = st.nextToken();
-//            switch(command){
-//                case ("L"):
-//                    if(cursor == 0){
-//                        break;
-//                    }
-//                    cursor--;
-//                    break;
-//                case ("D"):
-//                    if(cursor == answer.size()){
-//                        break;
-//                    }
-//                    cursor++;
-//                    break;
-//                case ("B"):
-//                    if(cursor == 0){
-//                        break;
-//                    }
-//                    answer.remove(cursor-1);
-//                    cursor--;
-//                    break;
-//                case ("P"):
-//                    char c = st.nextToken().charAt(0);
-//                    answer.add(cursor,c);
-//                    cursor++;
-//                    break;
-//            }
-//        }
-//
-//        for(char c : answer){
-//            sb.append(c);
-//        }
-//        System.out.println(sb);
-//    }
