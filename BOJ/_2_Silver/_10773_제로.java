551a61f85ab71666dbf13928a3a7fd182c8df81b
706com <ehdgusjdbswl@gmail.com>
2023-12-20 17:40:34 +0900

    review : 23-12-20

--- BOJ/_2_Silver/_10773_제로.java
+++ BOJ/_2_Silver/_10773_제로.java
@@ -5,7 +5,7 @@ package BOJ._2_Silver;
 //char 은 비교할 때 != '0' 형식 ' ' 작은 따옴표
 //String은 비교할 때 equals 형식 " " 큰 따옴표
 
-//+간단한 스택구현으로 해결.
+//소요시간 : 5분
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,70 +15,28 @@ import java.util.Stack;
 public class _10773_제로 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> st = new Stack<>();
         int K = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
 
         for(int i=0; i<K; i++){
-            int value = Integer.parseInt(br.readLine());
-            if(value != 0){
-                stack.push(value);
+            int num = Integer.parseInt(br.readLine());
+            if(num != 0){
+                st.push(num);
                 continue;
             }
-            stack.pop();
+            st.pop();
         }
 
-        int sum = 0;
-        while(!stack.isEmpty()){
-            sum += stack.pop();
-        }
-        System.out.println(sum);
+        int result = 0;
+//        int n = st.size();
+//        for(int i=0; i<n; i++){
+//            result += st.pop();
+//        }
 
+        // 위보다 이게 더 편한방법
+        while(!st.isEmpty()){
+            result += st.pop();
+        }
+        System.out.println(result);
     }
 }
-
-
-
-
-
-
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        int K = Integer.parseInt(br.readLine());
-//
-//        //입력된 수 모두를 빌더에 담아서 함수에 보내기 ex) sb=4 3 0 4 , sb=1 3 5 4 0 0 ~~
-//        for(int i=0; i<K; i++){
-//            sb.append(br.readLine()).append(" ");
-//        }
-//        int result = stackSum(K, sb.toString());
-//
-//        System.out.println(result);
-//
-//    }
-//    public static int stackSum(int K, String sb){
-//        //띄어쓰기 단위로 토큰화
-//        StringTokenizer st = new StringTokenizer(sb," ");
-//        Stack<Integer> stack = new Stack<>();
-//
-//        for(int i=0; i<K; i++){
-//            //
-//            String str = st.nextToken();
-//
-//            //스택을 이용하여 0이 아니면 int 형변환 후 push , 0이면 pop
-//            if(!str.equals("0")){
-//                stack.push(Integer.parseInt(str));
-//            }
-//            else if(str.equals("0")) {
-//                stack.pop();
-//            }
-//        }
-//
-//        int sum = 0;
-//        int size = stack.size();
-//
-//        //sum 에 담아서 리턴
-//        for(int i=0; i<size; i++){
-//            sum += stack.pop();
-//        }
-//        return sum;
-//    }
