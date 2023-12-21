830abd604c8a0c799d2311db37503e652849956f
706com <ehdgusjdbswl@gmail.com>
2023-12-21 12:37:06 +0900

    review : 23-12-21

--- BOJ/_2_Silver/_18258_큐2.java
+++ BOJ/_2_Silver/_18258_큐2.java
@@ -1,14 +1,7 @@
 package BOJ._2_Silver;
 //[백준]18528 : 큐2 - JAVA(자바)
 
-//< 나의 알고리즘 >
-// 양방향 출력을 해야하니 Dequeue 로 구현
-
-//< 답안 알고리즘 >
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
+//소요시간 : 6분
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,57 +10,56 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class _18528_큐2 {
+public class _18258_큐2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        Deque<Integer> deq = new LinkedList<>();
+        Deque<Integer> q = new LinkedList<>();
         int N = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
 
-            switch (command){
+            switch (command) {
                 case "push":
                     int num = Integer.parseInt(st.nextToken());
-                    deq.offer(num);
+                    q.offer(num);
                     break;
                 case "pop":
-                    if(deq.isEmpty()){
+                    if (q.isEmpty()) {
                         sb.append("-1").append('\n');
                         break;
                     }
-                    sb.append(deq.poll()).append('\n');
+                    sb.append(q.pollFirst()).append('\n');
                     break;
                 case "size":
-                    sb.append(deq.size()).append('\n');
+                    sb.append(q.size()).append('\n');
                     break;
                 case "empty":
-                    if(deq.isEmpty()){
+                    if (q.isEmpty()) {
                         sb.append("1").append('\n');
+                        break;
                     }
-                    else{
-                        sb.append("0").append('\n');
-                    }
+                    sb.append("0").append('\n');
                     break;
                 case "front":
-                    if(deq.isEmpty()){
+                    if (q.isEmpty()) {
                         sb.append("-1").append('\n');
                         break;
                     }
-                    sb.append(deq.peekFirst()).append('\n');
+                    sb.append(q.peekFirst()).append('\n');
                     break;
                 case "back":
-                    if(deq.isEmpty()){
+                    if (q.isEmpty()) {
                         sb.append("-1").append('\n');
                         break;
                     }
-                    sb.append(deq.peekLast()).append('\n');
+                    sb.append(q.peekLast()).append('\n');
                     break;
             }
         }
         System.out.println(sb);
     }
 }
+
