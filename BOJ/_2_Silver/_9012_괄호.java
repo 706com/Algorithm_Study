a3179295491babf34d688b8f64a472342298ab55
706com <ehdgusjdbswl@gmail.com>
2023-12-28 20:05:32 +0900

    review : 23-12-28

--- BOJ/_2_Silver/_9012_괄호.java
+++ BOJ/_2_Silver/_9012_괄호.java
@@ -1,50 +1,42 @@
 package BOJ._2_Silver;
 
+//소요시간 : 8분
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-//< 알고리즘 유형 >
-// 스택
-
-//< 알고리즘 풀이 >
-// 1) '(' 와 ')' 에 따른 stack 에 넣고 빼기!
-
 public class _9012_괄호 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        boolean check;
-
-        Stack<Character> stk = new Stack<>();
-
-        StringBuilder sb = new StringBuilder();
-
-        while (T-- > 0) {
-            check = false;
+        while(N-->0){
             String str = br.readLine();
+            Stack<Character> stk = new Stack<>();
+            boolean check = false;
+            for(int i=0; i<str.length(); i++){
+                char c = str.charAt(i);
 
-            for (int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                if (ch == '(') {
-                    stk.push(ch);
-                } else if (ch == ')') {
-                    if (stk.isEmpty()) {
-                        sb.append("NO").append('\n');
-                        check = true; // 남아있는게 없는데 ) 나왔다.
+                if(c=='('){
+                    stk.push(c);
+                }
+                else{
+                    if(!stk.isEmpty() && stk.peek() == '('){
+                        stk.pop();
+                    }
+                    else {
+                        check = true;
                         break;
                     }
-                    stk.pop();
                 }
             }
-            if (stk.isEmpty() && !check) {
-                sb.append("YES").append('\n');
-            } else if (!stk.isEmpty()) {
-                sb.append("NO").append('\n');
-                stk.removeAllElements();
+            if(!check && stk.isEmpty()){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
             }
         }
     }
