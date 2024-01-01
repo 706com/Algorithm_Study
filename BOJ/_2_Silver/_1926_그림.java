ee032c36e26272c5b4946aff875f526cdd3b7429
706com <ehdgusjdbswl@gmail.com>
2024-01-01 19:11:41 +0900

    24-01-01

---
+++ BOJ/_2_Silver/_1926_그림.java
@@ -0,0 +1,80 @@
+package BOJ._2_Silver;
+
+//소요시간 : 20분
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _1926_그림 {
+    static int N,M;
+    static int[][] arr;
+    static boolean[][] visited;
+
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0;
+        int max = 0;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(arr[i][j] == 1 && !visited[i][j]){
+                    max = Math.max(max,bfs(i,j));
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+        System.out.println(max);
+    }
+    static int bfs(int x, int y){
+        visited[x][y] = true;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x,y));
+        int count = 1;
+
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            for(int i=0; i<4; i++){
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=M){
+                    continue;
+                }
+                if(arr[nx][ny] == 0 || visited[nx][ny]){
+                    continue;
+                }
+                q.offer(new Node(nx,ny));
+                visited[nx][ny] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+    static class Node{
+        int x;
+        int y;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
