3b7f4e2b3865a02c32bf33e670c7cef4f5349c8b
706com <ehdgusjdbswl@gmail.com>
2024-01-09 22:12:08 +0900

    review : 24-01-09

--- BOJ/_2_Silver/_2667_단지번호붙이기.java
+++ BOJ/_2_Silver/_2667_단지번호붙이기.java
@@ -1,89 +1,87 @@
 package BOJ._2_Silver;
 //[백준]2667 : 단지번호붙이기 - JAVA(자바)
 
-//< 나의 알고리즘 >
-// 최단경로 문제가 아니니 dfs(재귀)로 구현
-
-//< 답안 알고리즘 >
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
+//소요시간 : 7분
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class _2667_단지번호붙이기 {
     static int N;
-    static int[][] board;
+    static int[][] arr;
     static boolean[][] visited;
     static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
 
-    static int dangi = 0;
-    static int count;
-    static ArrayList<Integer> arr = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
+
+        arr = new int[N][N];
         visited = new boolean[N][N];
 
-        //단지내용 입력
         for(int i=0; i<N; i++){
             String str = br.readLine();
             for(int j=0; j<N; j++){
-                int num = Character.getNumericValue(str.charAt(j));
-                board[i][j] = num;
+                arr[i][j] = str.charAt(j) -'0';
             }
         }
 
-        //dfs 를 돌려서 단지수 , 단지내 집 수 알아내기
-
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
-                // 단지가 있고(1), 방문하지 않았으면 dfs돌기
-                // dfs가 도는 갯수마다 단지 갯수 세기
-                // list 에 단지내부를 쭉 돌면서 센 count 넣어주고 count 초기화
-                if(board[i][j] == 1 && !visited[i][j]){
-                    dfs(i,j);
-                    dangi++;
-                    arr.add(count);
-                    count=0;
+                if(arr[i][j] == 1 && !visited[i][j]){
+                    list.add(bfs(i,j));
+                    count++;
                 }
             }
         }
-        // 단지내 집 수 오름차순 정렬
-        Collections.sort(arr);
+        Collections.sort(list);
 
-        //출력
-        System.out.println(dangi);
-
-        for(int i=0; i<arr.size(); i++){
-            System.out.println(arr.get(i));
+        System.out.println(count);
+        for(int x : list){
+            System.out.println(x);
         }
-
     }
-    static void dfs(int x,int y){
+    static int bfs(int x, int y){
+        Queue<Node> q= new LinkedList<>();
+        q.offer(new Node(x,y));
         visited[x][y] = true;
-        count++;
+        int count = 1;
 
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            for(int i=0; i<4; i++){
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
 
-            if(nx<0 || ny<0 || nx>=N || ny>=N)
-                continue;
+                if(nx<0 || nx>=N || ny<0 || ny>=N){
+                    continue;
+                }
+                if(arr[nx][ny] != 1 || visited[nx][ny]){
+                    continue;
+                }
+                q.offer(new Node(nx,ny));
+                visited[nx][ny] = true;
+                count++;
+            }
+        }
+        return count;
+    }
 
-            if(board[nx][ny] != 1 || visited[nx][ny])
-                continue;
-
-            dfs(nx,ny);
+    static class Node{
+        int x;
+        int y;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
