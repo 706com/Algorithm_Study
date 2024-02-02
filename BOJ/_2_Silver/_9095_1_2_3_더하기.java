0c2b64800532842f118534875153f25ffedc70ad
706com <ehdgusjdbswl@gmail.com>
2024-02-02 09:37:58 +0900

    review : 24-02-02

--- BOJ/_2_Silver/_9095_1_2_3_더하기.java
+++ BOJ/_2_Silver/_9095_1_2_3_더하기.java
@@ -1,41 +1,32 @@
 package BOJ._2_Silver;
 //[백준]9095 : 1,2,3 더하기 - JAVA(자바)
 
-//< 나의 알고리즘 >
-// DP 로 점화식을 구해서 bottom-up 방식으로 풀기
-
-//< 답안 알고리즘 >
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
+//소요시간 : 9분
+//DP : Bottom Up
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _9095_1_2_3_더하기 {
-    static int[] dp;
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        dp = new int[11];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
+        int[] dp = new int[11];
+        while(T-->0){
+            int N = Integer.parseInt(br.readLine());
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
 
-        for(int i=4; i<11; i++){
-            dp[i] = dp[i-1] + dp[i-2] +dp[i-3];
+            for(int i=4; i<=10; i++){
+                dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+            }
+            sb.append(dp[N]).append('\n');
         }
-
-//        for(int i=1; i<11; i++){
-//            System.out.print(dp[i]+" ");
-//        }
-
-        for(int i=0; i<T; i++){
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(dp[num]);
-        }
+        System.out.println(sb);
     }
 }
