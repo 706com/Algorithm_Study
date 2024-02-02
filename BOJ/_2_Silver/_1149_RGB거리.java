4f48605f209a2b29ff54d46d3f3bdaa8adecaae8
706com <ehdgusjdbswl@gmail.com>
2024-02-02 10:13:45 +0900

    review : 24-02-02

--- BOJ/_2_Silver/_1149_RGB거리.java
+++ BOJ/_2_Silver/_1149_RGB거리.java
@@ -1,15 +1,7 @@
 package BOJ._2_Silver;
 //[백준]1149 : RGB거리 - JAVA(자바)
 
-//< 나의 알고리즘 >
-// DP : bottom-up 으로, 2차원배열로 RGB 구성.
-
-
-//< 답안 알고리즘 >
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
+//소요시간 : 10분
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,41 +9,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _1149_RGB거리 {
-    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());    //집의 수
-        int[][] house = new int[N+1][4];
+        int N = Integer.parseInt(br.readLine());
 
-        dp = new int[N+1][4];
-
+        int[][] cost = new int[N+1][3];
+        int[][] dp = new int[N+1][3];
         for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            int red = Integer.parseInt(st.nextToken());
-            int green = Integer.parseInt(st.nextToken());
-            int blue = Integer.parseInt(st.nextToken());
-
-            house[i][1] = red;
-            house[i][2] = green;
-            house[i][3] = blue;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        dp[1][1] = house[1][1];
-        dp[1][2] = house[1][2];
-        dp[1][3] = house[1][3];
+        dp[1][0] = cost[1][0];  //R
+        dp[1][1] = cost[1][1];  //G
+        dp[1][2] = cost[1][2];  //B
 
-            //점화식
-            //dp[N][1] (red)일 때 : N-1 의 집의 blue, green 의 가격 중 작은것과 더하기.
-            //dp[N][2] (green)일 때 : N-1 의 집의 red, blue 의 가격 중 작은것과 더하기.
-            //dp[N][3] (blue)일 때 : N-1 의 집의 red, green 의 가격 중 작은것과 더하기.
-            for(int i=2; i<=N; i++){
-                dp[i][1] = Math.min(dp[i-1][2], dp[i-1][3]) + house[i][1];
-            dp[i][2] = Math.min(dp[i-1][1], dp[i-1][3]) + house[i][2];
-            dp[i][3] = Math.min(dp[i-1][1], dp[i-1][2]) + house[i][3];
+        for(int i=2; i<=N; i++){
+            dp[i][0] = cost[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);    //R(현재 집) + G,B(이전 집)중에서 택
+            dp[i][1] = cost[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);    //G(현재 집) + R,B(이전 집)중에서 택
+            dp[i][2] = cost[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);    //B(현재 집) + R,G(이전 집)중에서 택
         }
 
-        int result = Math.min( Math.min(dp[N][1],dp[N][2]) , dp[N][3]);
+        int result = Math.min(Math.min(dp[N][0],dp[N][1]),dp[N][2]);
         System.out.println(result);
     }
-
 }
