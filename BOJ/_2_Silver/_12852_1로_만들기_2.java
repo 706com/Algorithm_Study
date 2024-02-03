f81c94297b269f2866bc138fae07986c03381c30
706com <ehdgusjdbswl@gmail.com>
2024-02-03 17:21:38 +0900

    review : 24-02-03 📌

--- BOJ/_2_Silver/_12852_1로_만들기_2.java
+++ BOJ/_2_Silver/_12852_1로_만들기_2.java
@@ -1,77 +1,47 @@
 package BOJ._2_Silver;
-//< 나의 알고리즘 >
-// DP : Bottom-up 방식으로 풀기.
-// 경로저장 방법을 생각치 못해서 실패.
 
-//< 답안 알고리즘 >
-// DP 테이블 말고도, 추가적으로 경로를 저장하는 테이블을 만들어서 , 경로를 용이하게 관리할 것.
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
+//소요시간 : 30분
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _12852_1로_만들기_2 {
-    static int[] dp;
-    static int[] route;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
 
-        dp = new int[N+1];
-        route = new int[N+1];
+        int[] dp = new int[N+1];
+        int[] index = new int[N+1];
 
-        if(N==1){
-            System.out.println("0");
-            System.out.println("1");
-            return;
-        }
         dp[1] = 0;
-        dp[2] = 1;
+        index[1] = 1;
 
-        route[1] = 1;
-        route[2] = 1;
-
-        for(int i=3; i<=N; i++){
-
-            dp[i] = dp[i-1] +1;
-            route[i] = i-1;
-
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1]+1;
+            index[i] = i-1;
             if(i%2==0){
-                if(dp[i] > dp[i/2]+1){
-                    route[i] = i/2;
+                dp[i] = Math.min(dp[i],dp[i/2]+1);
+                //만일 dp[i]의 값이 변경 되었다면 경로 추적
+                if(dp[i] == dp[i/2]+1){
+                    index[i] = i/2;
                 }
-                dp[i] = Math.min(dp[i] , dp[i/2]+1);
             }
-
             if(i%3==0){
-                if(dp[i] > dp[i/3]+1){
-                    route[i] = i/3;
+                dp[i] = Math.min(dp[i],dp[i/3]+1);
+                //만일 dp[i]의 값이 변경 되었다면 경로 추적
+                if(dp[i] == dp[i/3]+1) {
+                    index[i] = i/3;
                 }
-                dp[i] = Math.min(dp[i] , dp[i/3]+1);
             }
         }
-
+        // 경로 따라 출력
         System.out.println(dp[N]);
-
-//        for(int i=1; i<=N; i++){
-//            System.out.print(route[i]+" ");
-//        }
-        int cur = N;
-        sb.append(cur+" ");
-        while(true){
-            cur = route[cur];
-            if(cur==1) {
-                sb.append("1"+" ");
-                break;
-            }
-            sb.append(cur+" ");
+        int target = N;
+        while(target != 1){
+            System.out.print(target+" ");
+            target = index[target];
         }
-        System.out.println(sb);
+        System.out.print(target);
     }
 }
