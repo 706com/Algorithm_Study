d080029f9d7f4279b715118b84df26710d3fe2e7
706com <ehdgusjdbswl@gmail.com>
2024-02-15 10:05:57 +0900

    review 24-02-15

--- BOJ/_2_Silver/_11399_ATM.java
+++ BOJ/_2_Silver/_11399_ATM.java
@@ -1,44 +1,32 @@
 package BOJ._2_Silver;
 //[백준]11399 : ATM - JAVA(자바)
 
-//< 알고리즘 유형 >
-// 그리디 , 정렬
+//소요시간 : 5분
 
-//< 알고리즘 풀이 >
-// dp 를 이용하여 더한 값들을 저장해놓는 방법 사용.
-
-
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
-
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class _11399_ATM {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int [N];
-        int[] dp = new int[N];
+        int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        dp[0] = arr[0];
-        int sum = dp[0];
-
-        for(int i=1; i<N; i++){
-            dp[i] = dp[i-1] + arr[i];
-            sum += dp[i];
+        int sum = 0;
+        int result = 0;
+        for(int x : arr){
+            sum += x;   //해당 사람이 기다린 시간
+            result += sum;  //총 인출시간
         }
-
-        System.out.println(sum);
+        System.out.println(result);
     }
-
 }
