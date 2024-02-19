38f328a3955f5b11078ea66e57775e7b384b5004
706com <ehdgusjdbswl@gmail.com>
2024-02-19 10:35:54 +0900

    review 24-02-19 🔍📌

--- BOJ/_2_Silver/_1541_잃어버린_괄호.java
+++ BOJ/_2_Silver/_1541_잃어버린_괄호.java
@@ -1,45 +1,48 @@
 package BOJ._2_Silver;
 //[백준]1541 : 잃어버린 괄호 - JAVA(자바)
 
-//< 알고리즘 유형 >
-// 그리디
+//소요시간 : 30M
 
-//< 알고리즘 풀이 >
-// split 혹은 StringTokenizer 활용
-// + 와 - 를 각각 분리하여 , 구분자 기준으로 잘라내어 계산.
-// 초깃값 설정 중요!
+// Integer.MAX_VALUE 로 초기화 해준 이유 ->
 
-//< 새로 알게된 것 >
-// st.hasMoreTokens();
+/*
+<만약 temp가 첫 번째, 두 번째, 세 번째 때 모두 23이 들어온다고 가정>
+그럼 처음 값으로는 23이니 sum = 23
+그 다음에는 if 조건절을 만족하지 않으니, sum -= temp에 의해 0이 될 것이다.
+그 다음이 문제. 만약 sum == 0을 조건절로 걸게 되면 sum=temp; 에 의해 sum은 23이 되어버려 잘못된 값을 갖게 된다.
+원래는 sum -= temp에 의해 -23 이 되어야 하는게 정상.
 
-//< 궁금한 것 >
+그래서 많은 문제들의 초기값은 중간 계산 혹은 결과로 나오지 않는 수로 초기화 해주는게 일반적인 방식이다!
+ */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _1541_잃어버린_괄호 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer sub = new StringTokenizer(br.readLine(),"-");   // '-' 로 토큰 분리
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str,"-");
 
         int result = Integer.MAX_VALUE;
 
-        while(sub.hasMoreTokens()){
+        while(st.hasMoreTokens()){
+            StringTokenizer st2 = new StringTokenizer(st.nextToken(),"+");
             int temp = 0;
-            StringTokenizer add = new StringTokenizer(sub.nextToken(),"+");
-
-            while(add.hasMoreTokens()){
-                temp += Integer.parseInt(add.nextToken());
+            while(st2.hasMoreTokens()){
+                temp += Integer.parseInt(st2.nextToken());
             }
 
+            //맨 처음이라는 뜻 == 맨 처음 초기화
             if(result == Integer.MAX_VALUE){
                 result = temp;
             }
-            else {
+            else{
                 result -= temp;
             }
         }
-
         System.out.println(result);
     }
 }
