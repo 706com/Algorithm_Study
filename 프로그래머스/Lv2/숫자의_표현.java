da0bb118faddfa8d0afd3d4c8ff8a8de7f6b0efb
706com <ehdgusjdbswl@gmail.com>
2024-04-01 10:35:24 +0900

    24-04-01

--- 프로그래머스/Lv2/숫자의_표현.java
+++ 프로그래머스/Lv2/숫자의_표현.java
@@ -7,10 +7,6 @@ public class 숫자의_표현 {
     class Solution {
         public int solution(int n) {
             int answer = 0;
-            int[] dp = new int[n+1];
-            for(int i=1; i<=n; i++){
-                dp[i] = i;
-            }
 
             for(int i=1; i<=n; i++){
                 int sum = 0;
