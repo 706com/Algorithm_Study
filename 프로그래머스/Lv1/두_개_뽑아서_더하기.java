a5db0e9c870abf034cb4590ebf586f699305d08e
706com <ehdgusjdbswl@gmail.com>
2024-04-17 18:47:30 +0900

    review : 24-04-17 📌

--- 프로그래머스/Lv1/두_개_뽑아서_더하기.java
+++ 프로그래머스/Lv1/두_개_뽑아서_더하기.java
@@ -20,4 +20,25 @@ public class 두_개_뽑아서_더하기 {
             return hs.stream().sorted().mapToInt(Integer::intValue).toArray();
         }
     }
+
+    /**
+     * TreeSet을 사용해서 정렬까지 한 번에
+     */
+//    class Solution {
+//        public int[] solution(int[] numbers) {
+//            int[] answer = {};
+//            TreeSet<Integer> ts = new TreeSet<>();
+//
+//            //1. 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더한다.
+//            //2. 모든 수를 배열에 담는다. (중복 X)
+//            //3. 오름차순으로 정렬한다.
+//            for(int i=0; i<numbers.length; i++){
+//                for(int j=i+1; j<numbers.length; j++){
+//                    ts.add(numbers[i] + numbers[j]);
+//                }
+//            }
+//
+//            //4. 반환한다.
+//            return ts.stream().mapToInt(Integer::intValue).toArray();
+//        }
 }
