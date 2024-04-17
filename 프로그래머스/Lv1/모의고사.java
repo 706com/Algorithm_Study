8c47227726d7033bfc90e783d2d035dedb1be5c8
706com <ehdgusjdbswl@gmail.com>
2024-04-17 20:31:55 +0900

    review : 24-04-17 🔍

--- 프로그래머스/Lv1/모의고사.java
+++ 프로그래머스/Lv1/모의고사.java
@@ -1,78 +1,63 @@
 package 프로그래머스.Lv1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+//소요시간 : 15분 (너무 깡 구현 느낌..?) + 스트림 리턴🔍
+
+import java.util.*;
 
 public class 모의고사 {
-    public int[] solution(int[] answers) {
-        int[] answer;
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {2,1,2,3,2,4,2,5};
-        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] correct = {0,0,0};
+    class Solution {
+        public int[] solution(int[] answers) {
+            List<Integer> list = new ArrayList<>();
+            int[] x = {1,2,3,4,5};
+            int[] y = {2,1,2,3,2,4,2,5};
+            int[] z = {3,3,1,1,2,2,4,4,5,5};
 
-        int pointOne = 0;
-        int pointTwo = 0;
-        int pointThree = 0;
+            int px = 0;
+            int py = 0;
+            int pz = 0;
 
-        List<Integer> list = new ArrayList<>();
+            int answerX = 0;
+            int answerY = 0;
+            int answerZ = 0;
 
-        for(int i=0; i<answers.length; i++){
-            if(i%5 == 0){
-                pointOne = 0;
+            for(int answer : answers){
+                if(px == x.length){
+                    px = 0;
+                }
+                if(py == y.length){
+                    py = 0;
+                }
+                if(pz == z.length){
+                    pz = 0;
+                }
+
+                if(x[px] == answer){
+                    answerX++;
+                }
+                if(y[py] == answer){
+                    answerY++;
+                }
+                if(z[pz] == answer){
+                    answerZ++;
+                }
+
+                px++;
+                py++;
+                pz++;
             }
-            if(i%8 == 0){
-                pointTwo = 0;
+            int max = Math.max(Math.max(answerX,answerY),answerZ);
+
+            if(max == answerX){
+                list.add(1);
             }
-            if(i%10 == 0){
-                pointThree = 0;
+            if(max == answerY){
+                list.add(2);
+            }
+            if(max == answerZ){
+                list.add(3);
             }
 
-            if(answers[i] == arr1[pointOne]){
-                correct[0]++;
-            }
-            if(answers[i] == arr2[pointTwo]){
-                correct[1]++;
-            }
-            if(answers[i] == arr3[pointThree]){
-                correct[2]++;
-            }
-            pointOne++;
-            pointTwo++;
-            pointThree++;
-
-            //5,8,10의 최소공배수 400(여기까지만 돌아도됨)
-            if(i == 400){
-                break;
-            }
+            return list.stream().mapToInt(Integer::intValue).toArray();
         }
-        //최댓값 뽑아내기
-        int max = Math.max(Math.max(correct[0],correct[1]),correct[2]);
-
-        //최댓값과 일치하는 수포자만 리스트에 넣기
-        for(int i=0; i<correct.length; i++){
-            if(correct[i] == max){
-                list.add(i+1);
-            }
-        }
-
-        answer = new int[list.size()];
-
-        //answer배열에 넣어주기
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-
-        System.out.println(Arrays.toString(answer));
-
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] answers = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-//        int[] answers = {1,3,2,4,2};
-        모의고사 sol = new 모의고사();
-        sol.solution(answers);
     }
 }
