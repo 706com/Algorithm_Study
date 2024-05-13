9ad9f3ba8613c9844556c60e9c4b2890c6949141
706com <ehdgusjdbswl@gmail.com>
2024-05-13 16:42:00 +0900

    review : 24-05-13 📌

--- 프로그래머스/Lv1/K번째수.java
+++ 프로그래머스/Lv1/K번째수.java
@@ -1,21 +1,50 @@
 package 프로그래머스.Lv1;
 //[프로그래머스] K번째수 - JAVA(자바)
 
-//< 알고리즘 유형 >
-// 정렬
-
-//< 알고리즘 풀이 >
-// 조건에 맞게 배열을 복사 및 쪼갠 후 답안 출력
+// 소요시간
+// [240513] : 20분
 
 //< 새로 알게된 것 >
 // Arrays.copyOfRange : split 과 같이 배열을 복사 후 인덱스대로 쪼갤 수 있다.
 
-//< 궁금한 것 >
 
-import java.util.Arrays;
+import java.util.*;
 
 public class K번째수 {
+
+    /**
+     * 시간적 측면은 나아졌으나.. 너무 복잡한 듯!
+     */
     public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        int tc = commands.length;
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        while(tc-->0){
+            int size = commands[count][1] - commands[count][0] + 1;
+            int index = commands[count][2] - 1;
+            Integer[] newArr = new Integer[size];
+
+            for(int i=commands[count][0]-1; i<=commands[count][1]-1; i++){
+
+                newArr[i-commands[count][0]+1] = array[i];
+            }
+            // newArr = Arrays.copyOfRange(array,commands[count][0],commands[count][1]);
+
+            Arrays.sort(newArr);
+
+            // System.out.println(Arrays.toString(newArr));
+
+            list.add(newArr[index]);
+
+            count++;
+        }
+        // return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+    public int[] solution2(int[] array, int[][] commands) {
         int[] answer = {};
         answer = new int[commands.length];
 
