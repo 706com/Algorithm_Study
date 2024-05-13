beaeaa42c95787705c97080b267955473e6f731c
706com <ehdgusjdbswl@gmail.com>
2024-05-13 14:02:24 +0900

    review : 24-05-13 📌🔍

--- 프로그래머스/Lv2/구명보트.java
+++ 프로그래머스/Lv2/구명보트.java
@@ -1,6 +1,8 @@
 package 프로그래머스.Lv2;
 
-//소요시간 : 34분
+//소요시간
+// [240311] : 34분 🔍
+// [240513] : 30분 🔍
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -8,22 +10,30 @@ import java.util.Collections;
 class 구명보트 {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Integer[] peoples = new Integer[people.length];
-        for(int i=0; i<people.length; i++){
-            peoples[i] = people[i];
-        }
+        // 1. 내림차순으로 정렬한다.
+        Integer[] peoples = Arrays.stream(people).boxed().toArray(Integer[]::new);
         Arrays.sort(peoples,Collections.reverseOrder());
+        // System.out.println(Arrays.toString(peoples));
 
-        int point = peoples.length-1;
-        for(int i=0; i<=point; i++){
-            if(peoples[i]+peoples[point]<=limit){
+        // 2. 투포인터 활용
+        int p = people.length -1 ;
+
+        for(int i=0; i<people.length; i++){
+            // 2-1) 작거나 같으면 pointer 내리기
+            if(peoples[i] + peoples[p]<=limit){
                 answer++;
-                point--;
+                p--;
             }
+            // 2-2) limit 보다 두개의 값이 크면 pointer냅두기
             else{
                 answer++;
             }
+            // 2-3) i > pointer 되는 순간 탈출
+            if(i>=p){
+                break;
+            }
         }
+
         return answer;
     }
 }
