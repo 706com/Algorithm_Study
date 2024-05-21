39a5cde7f94d73ffd7805dda29b792c37426cab1
706com <ehdgusjdbswl@gmail.com>
2024-05-21 09:05:03 +0900

    review : 24-05-21

--- 프로그래머스/Lv2/의상.java
+++ 프로그래머스/Lv2/의상.java
@@ -1,5 +1,7 @@
 package 프로그래머스.Lv2;
-//[프로그래머스] 의상 - JAVA(자바)
+
+//소요시간
+//[240521] : 20분
 
 //< 알고리즘 유형 >
 // 해쉬
@@ -7,40 +9,22 @@ package 프로그래머스.Lv2;
 //< 알고리즘 풀이 >
 //전체를 구하고, 아무것도 입지 않는경우 -1 을 해준다.
 
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
-
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 public class 의상 {
     public int solution(String[][] clothes) {
         int answer = 1;
+        HashMap<String,Integer> hm = new HashMap<>();
 
-        HashMap<String,Integer> map = new HashMap<>();
-
-        //key : 종류
-        //value : 의상
-        //key(종류)가 겹치면 그 key 에 의상들 갯수넣기(아예 착용안한것(NULL)도 포함하여 초깃값 1
-        for(int i=0; i< clothes.length; i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],1)+1);
+        for(int i=0; i<clothes.length; i++){
+            String type = clothes[i][1];
+            hm.put(type,hm.getOrDefault(type,1)+1);
         }
 
-        Iterator<Integer> it = map.values().iterator();
-        //해쉬에 담겨있는 모든 종류값 곱하기.
-        while(it.hasNext()){
-            answer *=it.next();
+        for(String x : hm.keySet()){
+            answer *= hm.get(x);
         }
 
-        //아무것도 안입었을때 -1 해주기
         return answer-1;
-
-    }
-    public static void main(String[] args) {
-        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-
-        의상 sol = new 의상();
-        System.out.println(sol.solution(clothes));
     }
 }
