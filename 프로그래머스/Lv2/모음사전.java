21a77063e33479969f6fd1717049aaaedb711c30
706com <ehdgusjdbswl@gmail.com>
2024-05-27 20:48:30 +0900

    review : 24-05-27 📌

--- 프로그래머스/Lv2/모음사전.java
+++ 프로그래머스/Lv2/모음사전.java
@@ -1,38 +1,52 @@
 package 프로그래머스.Lv2;
 
 //소요시간 : 59분
+//[240527] : 20분
 
-class 모음사전 {
+class Solution {
     static int N;
-    static String[] arr = {"A","E","I","O","U"};
-    static int answer = -1;
+    static String[] arr;
+    static String[] output;
+    static int count = 0;
     static boolean end = false;
-    StringBuffer sb = new StringBuffer();
     public int solution(String word) {
-        N = arr.length;
+        int answer = 0;
+        N = 5;
+        int len = word.length();
+        arr = new String[]{"A","E","I","O","U"};
+        output = new String[5];
 
-        dfs(0,0,word);
-        return answer;
+        dfs(0, 0,len,word);
+        return count -1;
     }
-    public void dfs(int start, int depth, String word){
-        answer++;
-        System.out.println(sb);
-        if(word.equals(sb.toString())){
-            System.out.println("correct"+" "+ answer);
-            end = true;
+
+    public void dfs(int start, int depth, int len,String word){
+        if(end){
             return;
         }
-        if(depth >= 5){
+        count++;
+        if(depth == len){
+            String str = "";
+            for(int i=0; i<depth; i++){
+                str += output[i];
+            }
+            // System.out.println(str+" : "+count);
+            if(str.equals(word)){
+                end = true;
+                return;
+            }
+        }
+        if(depth == N){
+            // System.out.println(Arrays.toString(output)+" : "+count);
             return;
         }
+
         for(int i=0; i<N; i++){
-            sb.append(arr[i]);
-            dfs(i,depth+1,word);
+            output[depth] = arr[i];
+            dfs(i,depth+1,len,word);
             if(end){
                 return;
             }
-            sb.deleteCharAt(depth);
         }
-
     }
 }
