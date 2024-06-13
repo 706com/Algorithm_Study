7de5a537405ee13219589e2eeaf0c34455dfb062
706com <ehdgusjdbswl@gmail.com>
2024-06-13 17:35:05 +0900

    review : 24-06-13 📌🔍

--- 프로그래머스/Lv3/단어_변환.java
+++ 프로그래머스/Lv3/단어_변환.java
@@ -1,11 +1,61 @@
 package 프로그래머스.Lv3;
 
 //소요시간 : 30분
+//[240613] : 17분 🔍
+
+import java.util.*;
 
 class 단어_변환 {
+
+    /*
+    * HashMap + Queue 풀이
+    * */
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        answer = bfs(begin, target,words);
+
+        return answer;
+    }
+    public int bfs(String begin,String target, String[] words){
+        Queue<String> q = new LinkedList<>();
+        q.offer(begin);
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put(begin,0);
+
+        while(!q.isEmpty()){
+            String curWord = q.poll();
+            for(int i=0; i<words.length; i++){
+                if(!hm.containsKey(words[i]) && check(curWord,words[i])){
+                    q.offer(words[i]);
+                    hm.put(words[i],hm.get(curWord)+1);
+                }
+            }
+            if(hm.containsKey(target)){
+                return hm.get(target);
+            }
+        }
+        return 0;
+    }
+    public boolean check(String curWord, String otherWord){
+        int count = 0;
+        for(int i=0; i<curWord.length(); i++){
+            if(curWord.charAt(i) != otherWord.charAt(i)){
+                count++;
+            }
+        }
+        if(count == 1){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    * DFS 풀이
+    * */
+
     static boolean[] visited;
     static int answer;
-    public int solution(String begin, String target, String[] words) {
+    public int solution2(String begin, String target, String[] words) {
         answer = Integer.MAX_VALUE;
         visited = new boolean[words.length];
 
@@ -30,7 +80,7 @@ class 단어_변환 {
             }
         }
     }
-    public boolean check(String begin, String word){
+    public boolean check2(String begin, String word){
         int count = 0;
         for(int i=0; i<begin.length(); i++){
             if(begin.charAt(i) != word.charAt(i)){
