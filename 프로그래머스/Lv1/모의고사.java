342b89432978d7cc28c11775c2dc64c4f9554a46
706com <ehdgusjdbswl@gmail.com>
2024-06-14 18:43:10 +0900

    review : 24-06-14

--- 프로그래머스/Lv1/모의고사.java
+++ 프로그래머스/Lv1/모의고사.java
@@ -1,63 +1,42 @@
 package 프로그래머스.Lv1;
 
 //소요시간 : 15분 (너무 깡 구현 느낌..?) + 스트림 리턴🔍
+//[240614] : 25분
 
 import java.util.*;
 
 public class 모의고사 {
-    class Solution {
-        public int[] solution(int[] answers) {
-            List<Integer> list = new ArrayList<>();
-            int[] x = {1,2,3,4,5};
-            int[] y = {2,1,2,3,2,4,2,5};
-            int[] z = {3,3,1,1,2,2,4,4,5,5};
+    public int[] solution(int[] answers) {
+        int[] answer1 = {1,2,3,4,5};
+        int[] answer2 = {2,1,2,3,2,4,2,5};
+        int[] answer3 = {3,3,1,1,2,2,4,4,5,5};
 
-            int px = 0;
-            int py = 0;
-            int pz = 0;
+        List<Integer> list = new ArrayList<>();
+        int count1 = 0; //수포자1 맞춘 횟수
+        int count2 = 0;
+        int count3 = 0;
 
-            int answerX = 0;
-            int answerY = 0;
-            int answerZ = 0;
-
-            for(int answer : answers){
-                if(px == x.length){
-                    px = 0;
-                }
-                if(py == y.length){
-                    py = 0;
-                }
-                if(pz == z.length){
-                    pz = 0;
-                }
-
-                if(x[px] == answer){
-                    answerX++;
-                }
-                if(y[py] == answer){
-                    answerY++;
-                }
-                if(z[pz] == answer){
-                    answerZ++;
-                }
-
-                px++;
-                py++;
-                pz++;
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == answer1[i%5]){
+                count1++;
             }
-            int max = Math.max(Math.max(answerX,answerY),answerZ);
-
-            if(max == answerX){
-                list.add(1);
+            if(answers[i] == answer2[i%8]){
+                count2++;
             }
-            if(max == answerY){
-                list.add(2);
+            if(answers[i] == answer3[i%10]){
+                count3++;
             }
-            if(max == answerZ){
-                list.add(3);
-            }
-
-            return list.stream().mapToInt(Integer::intValue).toArray();
         }
+
+        if(count1 >= count2 && count1 >= count3){
+            list.add(1);
+        }
+        if(count2 >= count1 && count2 >= count3){
+            list.add(2);
+        }
+        if(count3 >= count1 && count3 >= count2){
+            list.add(3);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
