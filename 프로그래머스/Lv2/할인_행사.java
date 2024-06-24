e140a9a659e1beaa761422e455801ce2fa43fb14
706com <ehdgusjdbswl@gmail.com>
2024-06-24 21:56:08 +0900

    review : 24-06-24

--- 프로그래머스/Lv2/할인_행사.java
+++ 프로그래머스/Lv2/할인_행사.java
@@ -2,49 +2,50 @@ package 프로그래머스.Lv2;
 
 // 소요시간
 // [240426] : 40분
+// [240624] : 40분
 
 import java.util.*;
 
 public class 할인_행사 {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-
-        // 행사 물품을 담는 해시맵을 생성한다.
         HashMap<String, Integer> hm = new HashMap<>();
-        int day = 0;
-        while(day != discount.length){
-            boolean isTrue = true;
 
-            //해당 일차의 할인 품목 담는다.
-            hm.put(discount[day],hm.getOrDefault(discount[day],0)+1);
-            //만일 10일이 넘어가면 day-10의 품목은 감소시킨다.(제거)
-            if(day>=10){
-                hm.put(discount[day-10],hm.getOrDefault(discount[day-10],0)-1);
+        //최초 10개
+        for(int i=0; i<10; i++){
+            hm.put(discount[i],hm.getOrDefault(discount[i],0)+1);
+        }
+
+        boolean isSale = true;
+        for(int i=0; i<want.length; i++){
+            // 하나라도 불일치하면 탈출
+            if(!hm.containsKey(want[i]) || hm.get(want[i]) != number[i]){
+                isSale = false;
+                break;
             }
-            day++;
-//             System.out.println("day : " + day);
-//             for(String x : hm.keySet()){
-//                 System.out.println(" key : " + x + " = " + hm.get(x));
-//             }
+        }
+        if(isSale){
+            answer++;
+        }
 
-            // 구매를 원하는 품목의 갯수를 보면서 result를 증가시킨다.
-            for(int i=0; i<want.length; i++){
-                //해당 품목이 없으면 탈출
-                if(!hm.containsKey(want[i])){
-                    isTrue = false;
+        //11번째부터 조회
+        for(int i=10; i<discount.length; i++){
+            isSale = true;
+            // 새로운 날짜 할인 품목 추가
+            hm.put(discount[i],hm.getOrDefault(discount[i],0)+1);
+            // 10일 전 할인 품목 제거
+            hm.put(discount[i-10],hm.getOrDefault(discount[i-10],0)-1);
+            for(int j=0; j<want.length; j++){
+                //📌 값이 존재하지 않거나, 하나라도 불일치하면 탈출
+                if(!hm.containsKey(want[j]) || hm.get(want[j]) != number[j]){
+                    isSale = false;
                     break;
                 }
-                //하나라도 갯수가 안되면 탈출
-                if(hm.get(want[i]) < number[i]){
-                    isTrue = false;
-                    break;
-                }
             }
-            if(isTrue){
+            if(isSale){
                 answer++;
             }
         }
-
         return answer;
     }
 }
