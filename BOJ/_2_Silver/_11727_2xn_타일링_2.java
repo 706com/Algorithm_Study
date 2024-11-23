1f24ff8808ed8390974f01b1f53486ffab9d1b10
706com <ehdgusjdbswl@gmail.com>
2024-11-23 20:37:25 +0900

    24-11-23

--- BOJ/_2_Silver/_11727_2xn_타일링_2.java
+++ BOJ/_2_Silver/_11727_2xn_타일링_2.java
@@ -1,26 +1,43 @@
 package BOJ._2_Silver;
 
 //소요시간 : 30분 (실패 : 점화식 못세움 + 경우의수 잘못셈)
+//[241123] : 10분
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class _11727_2xn_타일링_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         int[] dp = new int[N+1];
-
         dp[1] = 1;
-        if(N>=2) {
-            dp[2] = 3;
+        if(N==1){
+            System.out.println(dp[1]);
+            return;
         }
-
+        dp[2] = 3;
         for(int i=3; i<=N; i++){
-            dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+            dp[i] = (dp[i-1] + (2 * dp[i-2]) % 10007) % 10007;
         }
         System.out.println(dp[N]);
     }
 }
+
+
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//
+//        int[] dp = new int[N+1];
+//
+//        dp[1] = 1;
+//        if(N>=2) {
+//            dp[2] = 3;
+//        }
+//
+//        for(int i=3; i<=N; i++){
+//            dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+//        }
+//        System.out.println(dp[N]);
+//    }
