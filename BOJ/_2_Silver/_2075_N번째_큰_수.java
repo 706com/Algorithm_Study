5d9f92c85cb1310986259a6a000562a9cbd8a5d7
706com <ehdgusjdbswl@gmail.com>
2024-12-09 18:59:41 +0900

    24-12-09

---
+++ BOJ/_2_Silver/_2075_N번째_큰_수.java
@@ -0,0 +1,28 @@
+package BOJ._2_Silver;
+
+//[241209] 06:52 ~
+
+import java.io.*;
+import java.util.*;
+
+public class _2075_N번째_큰_수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int result = 0;
+        while(N-->0){
+            result = pq.poll();
+        }
+        System.out.println(result);
+    }
+}
