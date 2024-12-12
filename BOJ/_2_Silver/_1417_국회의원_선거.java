5f434406a12a68ce16f80ae38314f352c07bcd4f
706com <ehdgusjdbswl@gmail.com>
2024-12-13 01:36:44 +0900

    24-12-12

--- BOJ/_2_Silver/_1417_국회의원_선거.java
+++ BOJ/_2_Silver/_1417_국회의원_선거.java
@@ -1,7 +1,29 @@
 package BOJ._2_Silver;
 
-public class _1417_국회의원_선거 {
-    public static void main(String[] args) {
+//[241212] 10분
 
+import java.io.*;
+import java.util.*;
+
+public class _1417_국회의원_선거 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ds = Integer.parseInt(br.readLine());
+        int result = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<N-1; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        while(!pq.isEmpty()){
+            if(ds>pq.peek()){
+                break;
+            }
+            ds+=1;
+            result++;
+            pq.add(pq.poll()-1);
+        }
+        System.out.println(result);
     }
 }
