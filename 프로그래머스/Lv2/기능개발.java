c1c871ad2608d8fdc5764778153004c60d50c6ff
706com <ehdgusjdbswl@gmail.com>
2025-02-07 23:52:08 +0900

    25-02-07

--- 프로그래머스/Lv2/기능개발.java
+++ 프로그래머스/Lv2/기능개발.java
@@ -3,135 +3,52 @@ package 프로그래머스.Lv2;
 
 // <소요시간>
 // [240423] : 25분
+//[250207] 25분
 
-// 최적화 방법 : 클래스 안에 미리 구해둔 기능완료 날짜를 넣어둔다.
-// (int) Math.ceil(100.0/progresses[i]) / speeds[i] );
+//  return answerList.stream().mapToInt(Integer::intValue).toArray();
 
 import java.util.*;
 
 public class 기능개발 {
-    /**
-     * [240423]
-     * 최적화 X -> 최적화 필요
-     */
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        List<Integer> answerList = new ArrayList<>();
-        Queue<Node> q = new LinkedList<>();
+    public List<Integer> solution(int[] progresses, int[] speeds) {
 
-        //1. 클래스 안에 인덱스, 진행률, 스피드를 담는다.
-        //2. 클래스를 순서대로 큐에 담는다.
+        Queue<Node> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+
         for(int i=0; i<progresses.length; i++){
-            Node node = new Node(i,progresses[i],speeds[i]);
+            int remain = 100 - progresses[i];
+            int days = remain / speeds[i];
+            if(remain % speeds[i] != 0){
+                days++;
+            }
+            Node node = new Node(i,days);
             q.offer(node);
         }
+        int days = q.poll().days;
 
-        // for(Node x : q){
-        //     System.out.println(x);
-        // }
-
-        // q 내부를 살펴본다.
-
-        //3. 가장 상단의 인덱스를 기록한다.
-        int maxIdx = -1;
-
+        int count = 1;
         while(!q.isEmpty()){
-            int size = q.size();
-            int count = 0;
-            for(int i=0; i<size; i++){
-                Node node = q.poll();
-                node.prs += node.spd;
-                // System.out.println(node);
-                //-> 100이 되면 해당 인덱스 부터 뒤에 100이 끝날때까지 카운트
-                if(node.prs >= 100 && node.idx == maxIdx + 1){
-                    count++;
-                    maxIdx = node.idx;
-                }
-                //-> 100을 채우지 못하면 증가 후 뒤로
-                else{
-                    q.offer(node);
-                }
-            }
-            //4. 카운트가 끝나면 answer 리스트에 담는다.
-            if(count != 0){
-                answerList.add(count);
-            }
-        }
-        // System.out.println(answerList);
+            Node node = q.poll();
+            int nowDay = node.days;
 
-        //5. 리스트를 배열로 바꾼다.
-        return answerList.stream().mapToInt(Integer::intValue).toArray();
+            if(days>=nowDay){
+                count++;
+                continue;
+            }
+            days = nowDay;
+            list.add(count);
+            count=1;
+        }
+        list.add(count);
+        return list;
     }
+
     class Node{
         int idx;
-        int prs;
-        int spd;
-        Node(int i, int p,int s){
-            idx = i;
-            prs = p;
-            spd = s;
+        int days;
+        Node(int idx, int days){
+            this.idx = idx;
+            this.days= days;
         }
-        @Override
-        public String toString(){
-            return "i " + idx + " prs " + prs + " spd " + spd;
-        }
-    }
-/*-----------------------------------------*/
-    public int[] solution_(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        Deque<Integer> deq = new LinkedList<>();
-
-        for(int i=0; i<progresses.length; i++){
-            int remain = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i] >0){
-                remain +=1;
-            }
-            deq.add(remain);
-        }
-
-//        int size = deq.size();
-//        for(int i=0; i<size; i++){
-//            System.out.print(deq.poll()+" ");
-//        }
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while(!deq.isEmpty()) {
-            int count = 1 ;
-            int now = 0;
-            now = deq.poll();
-            do {
-                if(deq.isEmpty()){
-                    break;
-                }
-                if (deq.peek() <= now) {
-                    deq.poll();
-                    count++;
-                }
-            } while (!deq.isEmpty() && deq.peek() <= now );
-            list.add(count);
-        }
-
-//        for(int i=0; i<list.size(); i++){
-//            System.out.print(list.get(i)+" ");
-//        }
-
-        answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-
-        return answer;
-    }
-    public static void main(String[] args) {
-        int[] progresses = {93,30,55};
-        int[] speeds = {1,30,5};
-
-//        int[] progresses = {95, 90, 99, 99, 80, 99};
-//        int[] speeds = {1, 1, 1, 1, 1, 1};
-
-        기능개발 sol = new 기능개발();
-        sol.solution(progresses,speeds);
-
     }
 }
