8d2dc5a3d4c351a20ea7ecc5bcd7f8ccca7ea513
706com <ehdgusjdbswl@gmail.com>
2025-02-08 00:48:36 +0900

    25-02-08 📌🔍

--- 프로그래머스/Lv2/전화번호_목록.java
+++ 프로그래머스/Lv2/전화번호_목록.java
@@ -1,48 +1,25 @@
 package 프로그래머스.Lv2;
 //[프로그래머스] 전화번호 목록 - JAVA(자바)
 
-//< 알고리즘 유형 >
-// 해쉬
-
 //소요시간 : 26분 🔍📌
-
-//< 알고리즘 풀이 >
-// 1. 해쉬에 전화번호 담기.
-// 2. 각 전화번호의 substring 으로 해쉬에서 접두어 찾기.
-// 3. 접두어 있으면 false 없으면 true 리턴
+//[250208] 25분 🔍
 
 import java.util.*;
 
 public class 전화번호_목록 {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-
-        Map<String,Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
 
         for(String x : phone_book){
-            map.put(x,1);
+            set.add(x);
         }
-        for(String x : map.keySet()){
+        for(String x : phone_book){
             for(int i=1; i<x.length(); i++){
-                String subStr = x.substring(0,i);
-                if(map.containsKey(subStr)){
-                    answer = false;
-                    break;
+                if(set.contains(x.substring(0,i))){
+                    return false;
                 }
             }
-            if(!answer){
-                break;
-            }
         }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        //String[] phone_book = {"119", "97674223", "1195524421"};
-        //String[] phone_book = {"123","456","789"};
-        String[] phone_book = {"12","123","1235","567","88"};
-
-        전화번호_목록 sol = new 전화번호_목록();
-        System.out.println(sol.solution(phone_book));
+        return true;
     }
 }
