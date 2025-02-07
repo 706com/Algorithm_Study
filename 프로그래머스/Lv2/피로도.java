79b499c96b35308c5f2939df53ab35e97a052e89
706com <ehdgusjdbswl@gmail.com>
2025-02-08 01:29:05 +0900

    25-02-08 📌

--- 프로그래머스/Lv2/피로도.java
+++ 프로그래머스/Lv2/피로도.java
@@ -2,25 +2,28 @@ package 프로그래머스.Lv2;
 
 //[240229] : 1시간
 //[250121] : 🔍
+//[250208] : 29분
 
 // 접근 : 백트래킹
 
 class 피로도 {
-    static int max = 0;
-    boolean[] visited;
+    static int count = 0;
+    static boolean[] visited;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(k,0,dungeons);
-        return max;
-    }
-    public void dfs(int fatigue,int count,int[][] dungeons){
-        // System.out.println(fatigue);
-        max = Math.max(max, count);  // 최대 탐험 수 갱신
 
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && fatigue >= dungeons[i][0]) {
+        visited = new boolean[dungeons.length];
+
+        dfs(0,k,dungeons);
+        return count;
+    }
+    public void dfs(int depth,int k, int[][] dungeons){
+
+        count = Math.max(depth,count);
+
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && k>=dungeons[i][0]){
                 visited[i] = true;
-                dfs(fatigue - dungeons[i][1], count + 1, dungeons);
+                dfs(depth+1,k-dungeons[i][1],dungeons);
                 visited[i] = false;
             }
         }
