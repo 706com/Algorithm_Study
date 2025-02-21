057162225c648fabb380f45faec74ee51fee6f45
706com <ehdgusjdbswl@gmail.com>
2025-02-21 10:16:02 +0900

    25-02-21 📌🔍

---
+++ 프로그래머스/Lv3/광고_삽입.java
@@ -0,0 +1,68 @@
+package 프로그래머스.Lv3;
+
+//[250220] 🔍
+
+//슬라이딩 윈도우
+
+import java.util.*;
+
+public class 광고_삽입 {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+
+        int playTime = convertTime(play_time);
+        int advTime = convertTime(adv_time);
+
+        // System.out.println(playTime);
+        // System.out.println(advTime);
+
+        if(playTime == advTime){
+            return "00:00:00";
+        }
+        int[] times = new int[360000];  // 99:99:99
+        for(String log : logs){
+            StringTokenizer st = new StringTokenizer(log,"-");
+            int startTime = convertTime(st.nextToken());
+            int endTime = convertTime(st.nextToken());
+
+            // System.out.println(startTime+" "+endTime);
+
+            for(int i=startTime; i<endTime; i++){
+                times[i]++;
+            }
+        }
+
+        int startTime = 0;
+        long totalTime = 0;
+        long maxTotalTime = 0;
+
+        for(int i = advTime; i< playTime; i++){
+            totalTime += times[i] - times[i-advTime];
+            if(totalTime > maxTotalTime){
+                maxTotalTime = totalTime;
+                startTime = i - advTime + 1;
+            }
+        }
+
+        String timeStr = convertTimeStr(startTime);
+        // System.out.println(timeStr);
+
+        return timeStr;
+    }
+    public int convertTime(String time){
+        StringTokenizer st = new StringTokenizer(time,":");
+        return Integer.parseInt(st.nextToken()) * 3600 + Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
+    }
+    public String convertTimeStr(int time){
+        int h = time / 3600;
+        int m = (time % 3600) / 60;
+        int s = time % 3600 % 60;
+
+
+        String H = h>=10 ? String.valueOf(h) : "0"+String.valueOf(h);
+        String M = m>=10 ? String.valueOf(m) : "0"+String.valueOf(m);
+        String S = s>=10 ? String.valueOf(s) : "0"+String.valueOf(s);
+
+        return H+":"+M+":"+S;
+    }
+}
