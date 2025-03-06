db184bb0baf5a928b339fe306807df0ef244a2bb
706com <ehdgusjdbswl@gmail.com>
2025-03-06 20:33:13 +0900

    25-03-06

--- BOJ/_3_Gold/_1744_수_묶기.java
+++ BOJ/_3_Gold/_1744_수_묶기.java
@@ -5,75 +5,56 @@ package BOJ._3_Gold;
 
 //소요시간 : 40분(실패 : 반례실패)
 //소요시간 : 20분
+//[250306] : 15분
 
 // < 풀이 방법 >
 // 음수, 0, 양수 로 나눠서 풀이
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+
+import java.io.*;
+import java.util.*;
 
 public class _1744_수_묶기 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> plus = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minus = new PriorityQueue<>();
-        Queue<Integer> zero = new LinkedList<>();
+
+        PriorityQueue<Integer> minusPq = new PriorityQueue();
+        PriorityQueue<Integer> plusPq = new PriorityQueue(Collections.reverseOrder());
 
         for(int i=0; i<N; i++){
             int num = Integer.parseInt(br.readLine());
-            if(num>0){
-                plus.add(num);
-            }
-            else if(num == 0){
-                zero.add(num);
-            }
-            else{
-                minus.add(num);
+            if(num <= 0){
+                minusPq.offer(num);
+            } else{
+                plusPq.offer(num);
             }
         }
-        int result = 0;
-        while(true){
-            if(plus.size()>=2){
-                int now = plus.poll();
-                if(now == 1){
-                    result += now;
-                    continue;
-                }
-                else {
-                    if(plus.peek() == 1){
-                        result += now;
-                    }
-                    else{
-                        result += now * plus.poll();
-                    }
-                }
-            }
 
-            if(minus.size()>=2){
-                result += minus.poll() * minus.poll();
-            }
+        // 양수 처리
+        long result = 0;
+        while(plusPq.size() > 1){
+            int first = plusPq.poll();
+            int second = plusPq.peek();
 
-            if(plus.size() == 1){
-                result += plus.poll();
+            // 더한 것 보다 곱한게 클 경우 (1이 없을 경우)
+            if(first * second > first + second){
+                result += first * plusPq.poll();
             }
-            if(minus.size() == 1){
-                if(zero.size()!=0){
-                    result += minus.poll() * zero.poll();
-                }
-                else{
-                    result += minus.poll();
-                }
+            else{
+                result += first;
             }
+        }
+        if(!plusPq.isEmpty()){
+            result += plusPq.poll();
+        }
 
-            if(plus.size()==0 && minus.size()==0){
-                break;
-            }
+        // 음수 처리
+        while(minusPq.size() > 1){
+            result += (minusPq.poll() * minusPq.poll());
+        }
+        if(!minusPq.isEmpty()){
+            result += minusPq.poll();
         }
         System.out.println(result);
     }
