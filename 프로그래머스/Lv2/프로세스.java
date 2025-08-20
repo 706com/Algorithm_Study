d1a42793a18cf5ddda4c6e427cfc5e9d8515cab3
706com <ehdgusjdbswl@gmail.com>
2025-08-20 22:09:10 +0900

    25-08-20

--- 프로그래머스/Lv2/프로세스.java
+++ 프로그래머스/Lv2/프로세스.java
@@ -4,67 +4,45 @@ package 프로그래머스.Lv2;
 //< 알고리즘 유형 >
 // 스택/큐
 
-//< 알고리즘 풀이 >
-// 큐 안에 클래스로 저장시켜, 우선순위와 인덱스를 동시저장
+//[250820] : 12분
 
-//< 새로 알게된 것 >
-
-//< 궁금한 것 >
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class 프로세스 {
     public int solution(int[] priorities, int location) {
         Queue<Node> q = new LinkedList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-
-        int max = -1;
-        int answer = 1;
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0; i<priorities.length; i++){
-            q.offer(new Node(i,priorities[i]));
-            list.add(priorities[i]);
+            q.offer(new Node(priorities[i],i));
+            pq.offer(priorities[i]);
         }
 
-        Collections.sort(list,Collections.reverseOrder());
+        int cnt = 1;
         while(!q.isEmpty()){
             Node node = q.poll();
-            if(node.priority != list.get(0)){
-                q.offer(node);
+            //우선 순위가 해당 될 때
+            if(node.priority >= pq.peek()){
+                //내가 찾고 있는 것 일 때
+                if(node.idx == location){
+                    return cnt;
+                } else{
+                    cnt++;
+                    pq.poll();
+                    continue;
+                }
             }
-            else if(node.priority == list.get(0) && node.index != location){
-                list.remove(0);
-                answer++;
-            }
-            else if (node.priority == list.get(0) && node.index == location){
-                System.out.println(answer);
-                return answer;
-            }
+            //우선순위가 낮으면 뒤로 미루기
+            q.offer(node);
         }
-
-        return answer;
+        return -1;
     }
-
-    public static void main(String[] args) {
-//        int[] priorities = {2,1,3,2};
-        int[] priorities = {1,1,9,1,1,1};
-//        int location = 2;
-        int location = 0;
-
-
-        프로세스 sol = new 프로세스();
-        sol.solution(priorities,location);
-    }
-
-    static class Node{
-        int index;
+    class Node {
         int priority;
-        Node(int index,int priority){
-            this.index = index;
-            this.priority = priority;
+        int idx;
+
+        Node(int priorities, int idx) {
+            this.priority = priorities;
+            this.idx = idx;
         }
     }
 }
