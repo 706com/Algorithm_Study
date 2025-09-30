d85a7e47d261cf7c18fcd07f1c543f45418851f9
706com <ehdgusjdbswl@gmail.com>
2025-09-30 23:39:45 +0900

    25-09-30

--- BOJ/_2_Silver/_9012_괄호.java
+++ BOJ/_2_Silver/_9012_괄호.java
@@ -1,6 +1,7 @@
 package BOJ._2_Silver;
 
 //소요시간 : 8분
+//[250930] : 15분
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,36 +9,40 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class _9012_괄호 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        while(N-->0){
+        while(N-->0) {
+            String result = "";
+            boolean failFlag = false;
             String str = br.readLine();
             Stack<Character> stk = new Stack<>();
-            boolean check = false;
+
             for(int i=0; i<str.length(); i++){
                 char c = str.charAt(i);
 
                 if(c=='('){
                     stk.push(c);
-                }
-                else{
-                    if(!stk.isEmpty() && stk.peek() == '('){
-                        stk.pop();
-                    }
-                    else {
-                        check = true;
+                }else{
+                    // 실패조건 1 : ( 가 없음
+                    if(stk.isEmpty()){
+                        System.out.println("NO");
+                        failFlag = true;
                         break;
                     }
+                    stk.pop();
                 }
             }
-            if(!check && stk.isEmpty()){
-                System.out.println("YES");
+            if(failFlag){
+                continue;
             }
-            else{
+            //실패조건 2 : 끝났는데 ( 가 남아있음
+            if(!stk.isEmpty()){
                 System.out.println("NO");
+                continue;
             }
+            System.out.println("YES");
         }
     }
 }
