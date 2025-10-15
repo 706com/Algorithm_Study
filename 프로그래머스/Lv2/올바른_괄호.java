70be442ba96753b1c1b0b69af7b5d426aa31a10c
706com <ehdgusjdbswl@gmail.com>
2025-10-16 00:17:37 +0900

    25-10-15

--- 프로그래머스/Lv2/올바른_괄호.java
+++ 프로그래머스/Lv2/올바른_괄호.java
@@ -2,42 +2,36 @@ package 프로그래머스.Lv2;
 //[프로그래머스] 올바른 괄호 - JAVA(자바)
 
 //소요시간 : 5분
+//[251016] : 3분
 
-import java.util.Stack;
+import java.util.*;
 
 public class 올바른_괄호 {
     boolean solution(String s) {
         boolean answer = true;
+        Deque<Character> stk = new ArrayDeque<>();
 
-        Stack<Character> stk = new Stack<>();
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c=='('){
-                stk.push(c);
-            } else{
-                //예외처리1) 비어있으면 리턴
-                if(stk.isEmpty()){
-                    return false;
-                } else{
+
+            switch(c){
+                case '(':
+                    stk.push(c);
+                    break;
+                case ')':
+                    if(stk.isEmpty()){
+                        return false;
+                    }
                     stk.pop();
-                }
+                    break;
             }
         }
-        //예외처리2) 다 끝났는데 남아있으면 리턴
+
+        // 모두 순회하고 남아있으면 안됨
         if(!stk.isEmpty()){
             return false;
         }
 
         return true;
-    }
-    public static void main(String[] args) {
-        String s1 = "()()";
-        String s2 = "(())()";
-        String s3 = ")()(";
-        올바른_괄호 sol = new 올바른_괄호();
-        sol.solution(s1);
-        sol.solution(s2);
-        sol.solution(s3);
-
     }
 }
