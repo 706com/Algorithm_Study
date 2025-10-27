9fbbba4d42fa3d1109623f4c7443c5ac38232bb1
706com <ehdgusjdbswl@gmail.com>
2025-10-27 21:57:36 +0900

    25-10-27

--- 프로그래머스/Lv2/기능개발.java
+++ 프로그래머스/Lv2/기능개발.java
@@ -1,54 +1,58 @@
 package 프로그래머스.Lv2;
-//[프로그래머스] 같은 숫자는 싫어 - JAVA(자바)
 
 // <소요시간>
-// [240423] : 25분
-//[250207] 25분
-
-//  return answerList.stream().mapToInt(Integer::intValue).toArray();
+//[240423] : 25분
+//[250207] : 25분
+//[251027] : 16분
 
 import java.util.*;
 
 public class 기능개발 {
-    public List<Integer> solution(int[] progresses, int[] speeds) {
+    public int[] solution(int[] progresses, int[] speeds) {
 
-        Queue<Node> q = new LinkedList<>();
+        Queue<Node> q = new ArrayDeque<>();
+        for(int i=0; i<progresses.length; i++){
+            q.offer(new Node(progresses[i],speeds[i]));
+        }
+
         List<Integer> list = new ArrayList<>();
 
-        for(int i=0; i<progresses.length; i++){
-            int remain = 100 - progresses[i];
-            int days = remain / speeds[i];
-            if(remain % speeds[i] != 0){
-                days++;
+        // 최대 100일
+        for(int day=1; day<=100; day++){
+            if(q.isEmpty()){
+                break;
             }
-            Node node = new Node(i,days);
-            q.offer(node);
-        }
-        int days = q.poll().days;
-
-        int count = 1;
-        while(!q.isEmpty()){
-            Node node = q.poll();
-            int nowDay = node.days;
-
-            if(days>=nowDay){
-                count++;
-                continue;
+            //날짜 기준 poll()
+            int count = 0;
+            while(!q.isEmpty()){
+                Node nowNode = q.peek();
+                // 배포 시작
+                if(nowNode.progress + (nowNode.speed * day) >= 100){
+                    q.poll();
+                    count++;
+                } else{
+                    break;
+                }
             }
-            days = nowDay;
-            list.add(count);
-            count=1;
+            if(count != 0){
+                // System.out.println(day);
+                list.add(count);
+            }
         }
-        list.add(count);
-        return list;
+        // System.out.println(list);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
-
     class Node{
-        int idx;
-        int days;
-        Node(int idx, int days){
-            this.idx = idx;
-            this.days= days;
+        int progress;
+        int speed;
+        Node(int progress, int speed){
+            this.progress = progress;
+            this.speed = speed;
+        }
+        @Override
+        public String toString(){
+            return this.progress + " " + speed;
         }
     }
 }
