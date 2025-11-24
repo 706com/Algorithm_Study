7fb80d38ff88245b6dfedad3de0e5487bf9c10fa
706com <ehdgusjdbswl@gmail.com>
2025-11-24 23:28:38 +0900

    25-11-24

--- 프로그래머스/Lv3/베스트앨범.java
+++ 프로그래머스/Lv3/베스트앨범.java
@@ -1,101 +1,77 @@
 package 프로그래머스.Lv3;
 //[프로그래머스] 베스트앨범 - JAVA(자바)
 
-//< 알고리즘 유형 >
-// 해쉬
-
-//< 알고리즘 풀이 >
-// 오로지 해쉬로 풀려했으나 , 담아야 할 정보가 너무 많으니, class로 접근해야겠다..
-
-//< 새로 알게된 것 >
-// 람다식 : Collections.sort(albList, (o1,o2) -> o2.play - o1.play);
-
-//< 궁금한 것 >
-
+//[251124] 34분
 
 import java.util.*;
 
 public class 베스트앨범 {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int[] answer = {};
 
-        // 장르중 plays 수의 총 합 담기 (가장 많이 들은 종류 담기 위해)
+        List<Integer> result = new ArrayList<>();
+
+        Map<String,Integer> score = new HashMap<>();
+        Map<String,PriorityQueue<Node>> album = new HashMap<>();
+
+        // 1. 장르 순회 및 적재
         for(int i=0; i<genres.length; i++){
-            map.put(genres[i], map.getOrDefault(genres[i],0)+plays[i]);
+            String genre = genres[i];
+            int play = plays[i];
+
+            //점수 책정
+            score.put(genre,score.getOrDefault(genre,0)+play);
+
+            //앨범 장르별 적재
+            if(!album.containsKey(genre)){
+                PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)-> {
+                    // 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록
+                    if(o1.plays==o2.plays){
+                        return o1.idx - o2.idx;
+                    }
+                    //2. 장르 내에서 많이 재생된 노래를 먼저 수록
+                    return o2.plays - o1.plays;
+                });
+                album.put(genre, pq);
+            }
+            album.get(genre).add(new Node(i,genre,play));
         }
 
-        // 1. 장르 선정하기
-        ArrayList<String> list = new ArrayList<>();
-        while(!map.isEmpty()){
-            int max = Integer.MIN_VALUE;
-            String maxKey = "";
+        //2. score에서 장르 순위 매기기
+        List<Node> list = new ArrayList<>();
+        for(String x : score.keySet()){
+            Node node = new Node(0,x,score.get(x));
+            list.add(node);
+        }
+        Collections.sort(list,(o1,o2)-> o2.plays - o1.plays);
+        // System.out.println(list);
 
-            for(String key : map.keySet()){
-                int temp = map.get(key);
-                if(max<temp){
-                    max = temp;
-                    maxKey = key;
+        //3. 순위별로 앨범 수록하기 (최대 2개)
+        for(Node x : list){
+            String genre = x.genre;
+            PriorityQueue<Node> pq = album.get(genre);
+            int count = 0;
+            while(!pq.isEmpty()){
+                if(count==2){
+                    break;
                 }
-            }
-            //리스트에 크기 별 장르 넣고 , hash에서는 삭제
-            list.add(maxKey);
-            map.remove(maxKey);
-        }
-
-//        //리스트에 크기 별 장르  출력
-//        for(int i=0; i<list.size(); i++){
-//            System.out.println(list.get(i)+" ");
-//        }
-
-        // 2.장르 내 노래 선정
-        ArrayList<Album> result = new ArrayList<>();
-
-        // 리스트에 클래스 담기
-        for(String genr : list){
-            ArrayList<Album> albList = new ArrayList<>();
-            for(int i=0; i< genres.length; i++){
-                if(genres[i].equals(genr)){
-                    albList.add(new Album(i,genres[i],plays[i]));
-                }
-            }
-
-            //내림차순 정렬 (장르) 중 plays수에 따라
-            Collections.sort(albList, (o1,o2) -> o2.play - o1.play);
-
-            //한개는 무조건 담고, 앨범 내 수록곡이 2개 이상이면 하나 더 담기
-            result.add(albList.get(0));
-            if(albList.size()>1){
-                result.add(albList.get(1));
+                result.add(pq.poll().idx);
+                count++;
             }
         }
-
-        answer = new int[result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i).index;
-        }
-
-//        for(int i=0; i<result.size(); i++){
-//            System.out.println(answer[i]+" ");
-//        }
-        return answer;
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
-    public static void main(String[] args) {
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
-
-        베스트앨범 sol = new 베스트앨범();
-        System.out.println(sol.solution(genres,plays));
-
-    }
-    static class Album{
-        int index;
+    class Node{
+        int idx;
         String genre;
-        int play;
-        Album(int index, String genre , int play){
-            this.index=index;
+        int plays;
+        Node(int idx,String genre, int plays){
+            this.idx = idx;
             this.genre = genre;
-            this.play = play;
+            this.plays = plays;
+        }
+        public String toString(){
+            return idx +" "+genre+" "+ plays;
         }
     }
 }
+
