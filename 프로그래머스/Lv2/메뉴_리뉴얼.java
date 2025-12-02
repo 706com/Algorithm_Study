a605384468ca1291d09b4eb57dc2a3fc6bc002d0
706com <ehdgusjdbswl@gmail.com>
2025-12-02 23:29:38 +0900

    25-12-02 🔎

--- 프로그래머스/Lv2/메뉴_리뉴얼.java
+++ 프로그래머스/Lv2/메뉴_리뉴얼.java
@@ -1,59 +1,76 @@
 package 프로그래머스.Lv2;
 
 //[250225] 1시간 📌
+//[251202] 1시간 53분 🔎
 
 import java.util.*;
 
 public class 메뉴_리뉴얼 {
     Map<String,Integer> map = new HashMap<>();
+    boolean[] visited;
+    int minCourse;
 
     public String[] solution(String[] orders, int[] course) {
-        // String[] answer = {};
-
         for(int i=0; i<orders.length; i++){
-            boolean[] visited = new boolean[orders[i].length()];
-            //코스요리 알파벳 정렬
+            visited = new boolean[orders[i].length()];
+            minCourse = course[0];
+
             char[] arr = orders[i].toCharArray();
             Arrays.sort(arr);
-            orders[i] = new String(arr);
+            String sortedOrder = new String(arr);
 
-            // 모든 조합 계산
-            combination(0,"",orders[i],visited);
+            combination(0,0,"",sortedOrder);
         }
-
-        //value 기준 내림차순 정렬
-        List<String> list = new ArrayList<>(map.keySet());
-        Collections.sort(list,(o1,o2)->(map.get(o2)-map.get(o1)));
-
-        List<String> answer = new ArrayList<>();
-
-        //최소 2개 이상이여야 하고, course 수 중에서 가장 크거나 같은것
+        // 각 코스 요리 중, 최다 주문 횟수 발췌
+        Map<Integer,Integer> mostOrderCount = new HashMap<>();
         for(int i=0; i<course.length; i++){
-            int max = 2;
-            for(String x : list){
-                if(x.length() == course[i] && max <= map.get(x)){
-                    max = map.get(x);
-                    answer.add(x);
+            for(String x : map.keySet()){
+                if(x.length() == course[i]){
+                    if(!mostOrderCount.containsKey(course[i])){
+                        mostOrderCount.put(course[i],0);
+                    }
+                    int max = Math.max(mostOrderCount.get(course[i]),map.get(x));
+                    mostOrderCount.put(course[i],max);
                 }
             }
         }
-        Collections.sort(answer);
-        // System.out.println(answer);
-        return answer.toArray(new String[0]);   //String List -> Arr
-    }
-    public void combination(int depth,String str, String order,boolean[] visited){
-        // System.out.println(str);
 
-        //2자리 이상
-        if(str.length() != 1){
-            map.put(str,map.getOrDefault(str,0)+1);
-        }
-        for(int i=depth; i<order.length(); i++){
-            if(!visited[i]){
-                visited[i] = true;
-                combination(i+1,str+order.charAt(i),order,visited);
-                visited[i] = false;
+        // for(int x : mostOrderCount.keySet()){
+        //     System.out.println(x +" : "+ mostOrderCount.get(x));
+        // }
+
+        //만일, 코스요리의 최다 갯수에 포함이 된다면 result 담기
+        //1명한테 받은건 안됨
+        List<String> result = new ArrayList<>();
+        for(String x : map.keySet()){
+            if(map.get(x) == mostOrderCount.get(x.length()) && mostOrderCount.get(x.length()) != 1){
+                result.add(x);
             }
+        }
+
+        Collections.sort(result);
+        // System.out.println(result);
+
+        return result.stream().toArray(String[]::new);
+    }
+
+    public void combination(int start,int depth, String now, String origin){
+        if(depth > origin.length()){
+            return;
+        }
+        // System.out.println(now);
+
+        if(minCourse <= now.length()){
+            map.put(now,map.getOrDefault(now,0)+1);
+        }
+
+        for(int i=start; i<origin.length(); i++){
+            if(visited[i]){
+                continue;
+            }
+            visited[i] = true;
+            combination(i+1,depth+1,now+String.valueOf(origin.charAt(i)),origin);
+            visited[i] = false;
         }
     }
 }
